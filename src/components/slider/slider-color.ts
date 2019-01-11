058a5f5d04ebb11cdc48c08486bfad62467f8f19
Andrew Tran <atran@adobe.com>
2019-01-11 14:18:01 -0800

    Converts slider to litElement

--- src/components/slider/slider-color.ts
+++ src/components/slider/slider-color.ts
@@ -10,6 +10,8 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+import { html, LitElement, property } from '@polymer/lit-element';
+
 // @ts-ignore - css generated at build time
 import sliderColorStyles from './slider-color.css.js';
 // @ts-ignore - css generated at build time
@@ -17,171 +19,60 @@ import sliderSkinStyles from './slider-skin.css.js';
 // @ts-ignore - css generated at build time
 import sliderStyles from './slider.css.js';
 
-export class SpectrumSliderColor extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        if (!this.shadowRoot) {
-            throw new Error('Failed to attach ShadowRoot!');
-        }
-        this.shadowRoot!.innerHTML = this.render();
+export class SpectrumSliderColor extends LitElement {
+    @property({ type: String })
+    public type = '';
 
-        this.updateView();
+    @property({ type: Number })
+    public value = 10;
 
-        this.shadowRoot!.getElementById('input')!.oninput = (event) => {
-            const inputValue = (this.shadowRoot!.getElementById(
-                'input'
-            ) as HTMLInputElement).value;
+    @property({ type: String })
+    public label = '';
 
-            this.value = parseFloat(inputValue);
-            this.updateView();
-        };
+    @property({ type: Number })
+    public max = 20;
 
-        this.shadowRoot!.getElementById('input')!.onmousedown = (event) => {
-            this.shadowRoot!.getElementById('handle')!.classList.add(
-                'is-dragged'
-            );
-        };
-        this.shadowRoot!.getElementById('input')!.onmouseup = (event) => {
-            this.shadowRoot!.getElementById('handle')!.classList.remove(
-                'is-dragged'
-            );
-        };
+    @property({ type: Number })
+    public min = 0;
+
+    @property({ type: Number })
+    public step = 1;
+
+    @property({ type: Boolean })
+    public disabled = false;
+
+    @property({ type: Boolean })
+    public isDragging = false;
+
+    private onInput(value: string) {
+        this.value = parseFloat(value);
     }
 
-    private updateView() {
-        const handle = this.shadowRoot!.getElementById('handle');
-        const valueLabel = this.shadowRoot!.getElementById('value');
-        const handleOffset = `${this.trackProgress * 100}%`;
-
-        handle!.style.left = handleOffset;
-        valueLabel!.innerHTML = this.value.toString();
+    private onMouseDown() {
+        this.isDragging = true;
     }
 
-    /**
-     * Getter for type attribute
-     */
-    public get type(): string | null {
-        return this.getAttribute('type');
+    private onMouseUp() {
+        this.isDragging = false;
+    }
+
+    private get inputElement() {
+        return this.shadowRoot!.getElementById('input') as HTMLInputElement;
     }
 
     /**
-     * Setter for type attribute
+     * Ratio representing the slider's position on the track
      */
-    public set type(value: string | null) {
-        if (value) {
-            this.setAttribute('type', value);
-        } else {
-            this.removeAttribute('type');
-        }
+    private get trackProgress(): number {
+        return this.value / this.max;
     }
 
-    /**
-     * Getter for max attribute
-     */
-    public get value(): number {
-        return parseFloat(this.getAttribute('value') || '10');
+    private get handleStyle(): string {
+        return `left: ${this.trackProgress * 100}%`;
     }
 
-    /**
-     * Getter for max attribute
-     */
-    public set value(value: number) {
-        if (value !== null) {
-            this.setAttribute('value', value.toString());
-        } else {
-            this.removeAttribute('value');
-        }
-    }
-
-    /**
-     * Getter for type attribute
-     */
-    public get label(): string {
-        return this.getAttribute('label') || '';
-    }
-
-    /**
-     * Setter for type attribute
-     */
-    public set label(value: string) {
-        if (value) {
-            this.setAttribute('label', value);
-        } else {
-            this.removeAttribute('label');
-        }
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public get max(): number {
-        return parseFloat(this.getAttribute('max') || '10');
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public set max(value: number) {
-        if (value) {
-            this.setAttribute('max', value.toString());
-        } else {
-            this.removeAttribute('max');
-        }
-    }
-
-    /**
-     * Getter for min attribute
-     */
-    public get min(): number {
-        return parseFloat(this.getAttribute('min') || '0');
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public set min(value: number) {
-        if (value) {
-            this.setAttribute('min', value.toString());
-        } else {
-            this.removeAttribute('min');
-        }
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public get step(): number {
-        return parseFloat(this.getAttribute('step') || '1');
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public set step(value: number) {
-        if (value) {
-            this.setAttribute('step', value.toString());
-        } else {
-            this.removeAttribute('step');
-        }
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public get disabled(): boolean {
-        return this.getAttribute('isDisabled') ? true : false;
-    }
-
-    /**
-     * Getter for max attribute
-     */
-    public set disabled(value: boolean) {
-        if (value) {
-            this.setAttribute('isDisabled', value.toString());
-        } else {
-            this.removeAttribute('isDisabled');
-        }
+    private get handleClass(): string {
+        return this.isDragging ? 'is-dragged' : '';
     }
 
     /**
@@ -192,7 +83,7 @@ export class SpectrumSliderColor extends HTMLElement {
     }
 
     private render() {
-        return `
+        return html`
             <style>
                 ${sliderStyles}
                 ${sliderSkinStyles}
@@ -211,9 +102,16 @@ export class SpectrumSliderColor extends HTMLElement {
                       step="${this.step}"
                       min="${this.min}"
                       max="${this.max}"
+                      @input=${(event: object) =>
+                          this.onInput(this.inputElement!.value)}
+                      @mousedown=${(event: object) => this.onMouseDown()}
+                      @mouseup=${(event: object) => this.onMouseUp()}
                   />
                 <div class="track"></div>
-                <div id="handle"></div>
+                <div id="handle"
+                    class="${this.handleClass}"
+                    style="${this.handleStyle}"
+                ></div>
                 </div>
             </div>
         `;
