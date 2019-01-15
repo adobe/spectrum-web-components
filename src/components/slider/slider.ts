96f5ed384fad1f942c5f6ce4acce6de08b058a33
Andrew Tran <atran@adobe.com>
2019-01-15 13:13:37 -0800

    Changes slider event detail to the current value of the slider

--- src/components/slider/slider.ts
+++ src/components/slider/slider.ts
@@ -54,24 +54,33 @@ export class SpectrumSlider extends LitElement {
 
         this.value = parseFloat(inputValue);
 
-        const inputEvent = new CustomEvent('slider-input', {
+        interface ISliderInputEventDetail {
+            detail: number;
+        }
+
+        const inputEventInit = <ISliderInputEventDetail>{
             bubbles: true,
             composed: true,
-            detail: ev,
-        });
+            detail: this.value,
+        };
 
-        inputEvent.value = this.value;
+        const inputEvent = new CustomEvent('slider-input', inputEventInit);
+
         this.dispatchEvent(inputEvent);
     }
 
     public onChange(ev: Event) {
-        const changeEvent = new CustomEvent('slider-change', {
+        interface ISliderChangeEventDetail {
+            detail: number;
+        }
+
+        const changeEventInit = <ISliderChangeEventDetail>{
             bubbles: true,
             composed: true,
-            detail: ev,
-        });
+            detail: this.value,
+        };
 
-        changeEvent.value = this.value;
+        const changeEvent = new CustomEvent('slider-change', changeEventInit);
         this.dispatchEvent(changeEvent);
     }
 
