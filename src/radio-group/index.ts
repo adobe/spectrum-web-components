cd1805e5254bf0b58125d85200952456c92310bf
Andrew Tran <atran@adobe.com>
2019-02-28 14:31:33 -0800

    Removes demo pages from export

--- src/radio-group/index.ts
+++ src/radio-group/index.ts
@@ -10,4 +10,3 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 export * from './radio-group';
-export * from './radio-group-demo';
