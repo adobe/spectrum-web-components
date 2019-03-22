0513b55f5801416fc0eac65d6c5c1a573a867571
Jiamin Sun <jisun@jiamins-mbp.corp.adobe.com>
2019-03-22 13:53:30 -0700

    direction updated

--- src/tab-list/tab-list-demo.ts
+++ src/tab-list/tab-list-demo.ts
@@ -10,13 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import {
-    html,
-    LitElement,
-    property,
-    CSSResultArray,
-    TemplateResult,
-} from 'lit-element';
+import { html, LitElement, property, TemplateResult } from 'lit-element';
 
 import { defineCustomElements, Tab } from '../index.js';
 
