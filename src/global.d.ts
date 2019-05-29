0b37a2b5912c60c5fc1fcd241e151a298eacd6a5
Andrew Tran <atran@adobe.com>
2019-05-29 13:19:48 -0700

    Imports CSSResultArray in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,6 +10,8 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+import { CSSResultArray } from 'lit-element';
+
 declare module '*.css' {
     const content: CSSResultArray;
     export default content;
