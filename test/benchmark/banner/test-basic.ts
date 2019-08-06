009f10c3e8a82b887d3f3a4427c28b185a97dbc3
Westbrook Johnson <wesjohns@wesjohns-macos.macromedia.com>
2019-08-06 09:42:53 -0700

    WIP: metering

---
+++ test/benchmark/banner/test-basic.ts
@@ -0,0 +1,10 @@
+import '../../../lib/banner/index.js';
+import { html } from 'lit-html';
+import { measureFixtureCreation } from '../helpers';
+
+measureFixtureCreation(html`
+    <sp-banner type="info">
+        <div slot="header">Info Text</div>
+        <div slot="content">Info Content</div>
+    </sp-banner>
+`);
