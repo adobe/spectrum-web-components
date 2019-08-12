8d6624f278c173cc98aafed77db880d007e585fe
Westbrook <westbrook.johnson@gmail.com>
2019-08-12 14:11:00 -0400

    Clean up

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,14 @@
-// /*
-// Copyright 2018 Adobe. All rights reserved.
-// This file is licensed to you under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License. You may obtain a copy
-// of the License at http://www.apache.org/licenses/LICENSE-2.0
+/*
+Copyright 2018 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-// Unless required by applicable law or agreed to in writing, software distributed under
-// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-// OF ANY KIND, either express or implied. See the License for the specific language
-// governing permissions and limitations under the License.
-// */
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require('deepmerge');
 const path = require('path');
@@ -17,11 +17,6 @@ module.exports = (config) => {
     config.set(
         merge(createDefaultConfig(config), {
             files: [
-                // runs all files ending with .test in the test folder,
-                // can be overwritten by passing a --grep flag. examples:
-                //
-                // npm run test -- --grep test/foo/bar.test.js
-                // npm run test -- --grep test/bar/*
                 {
                     pattern: config.grep ? config.grep : 'test/**/*.spec.js',
                     type: 'module',
@@ -29,9 +24,7 @@ module.exports = (config) => {
             ],
             reporters: ['junit'],
 
-            // see the karma-esm docs for all options
             esm: {
-                // if you are using 'bare module imports' you will need this option
                 nodeResolve: true,
             },
             browsers: [
