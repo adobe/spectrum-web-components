15b95f901c03c1b15a422ca579a0610d840488c9
Westbrook <westbrook.johnson@gmail.com>
2019-08-12 11:46:16 -0400

    Massage test for CI

--- test/iconset.spec.ts
+++ test/iconset.spec.ts
@@ -59,9 +59,13 @@ describe('Iconset', () => {
         );
 
         const icon = el.querySelector('sp-icon') as Icon | null;
+<<<<<<< HEAD
         const iconSet = el.querySelector('sp-icons-medium') as Icon | null;
 
         await elementUpdated(iconSet!);
+=======
+
+>>>>>>> Massage test for CI
         await elementUpdated(icon!);
 
         const svg = icon!.shadowRoot!.querySelector('[role="img"]');
