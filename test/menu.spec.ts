2278582cab98964cc1322602879eebc9d320dbd6
Westbrook <westbrook.johnson@gmail.com>
2019-08-13 16:30:54 -0400

    Add tests speculatively.

--- test/menu.spec.ts
+++ test/menu.spec.ts
@@ -16,11 +16,12 @@ import { Menu } from '../src/menu';
 import '../src/menu';
 import '../src/menu-item';
 import { html } from 'lit-element';
+import { MenuItem } from '../src/menu-item/menu-item';
 
 chai.use(chaiDomDiff);
 
-describe('Link', () => {
-    it('loads', async () => {
+describe('Menu', () => {
+    it('renders', async () => {
         const el = await fixture<Menu>(
             html`
                 <sp-menu>
@@ -48,5 +49,79 @@ describe('Link', () => {
         );
 
         await elementUpdated(el);
+
+        // TODO: Should probably be snapshots, but wait till #115 is merged to figure out why they don't work here.
+        expect(el.querySelectorAll('sp-menu-item').length).to.equal(6);
+    });
+
+    it('handle focus and keyboard input', async () => {
+        const handleFocus = (e: Event) => {
+            activeElement = e.target as MenuItem;
+        };
+        const el = await fixture<Menu>(
+            html`
+                <sp-menu @focusin=${handleFocus}>
+                    <sp-menu-item>
+                        Deselect
+                    </sp-menu-item>
+                    <sp-menu-item>
+                        Select Inverse
+                    </sp-menu-item>
+                    <sp-menu-item>
+                        Feather...
+                    </sp-menu-item>
+                    <sp-menu-item>
+                        Select and Mask...
+                    </sp-menu-item>
+                    <sp-menu-divider></sp-menu-divider>
+                    <sp-menu-item>
+                        Save Selection
+                    </sp-menu-item>
+                    <sp-menu-item disabled aria-disabled="true">
+                        Make Work Path
+                    </sp-menu-item>
+                </sp-menu>
+            `
+        );
+
+        await elementUpdated(el);
+
+        let activeElement = el.querySelector(
+            'sp-menu-item:nth-of-type(4)'
+        ) as MenuItem;
+        const firstItem = el.querySelector(
+            'sp-menu-item:nth-of-type(1)'
+        ) as MenuItem;
+        const thirdToLastItem = el.querySelector(
+            'sp-menu-item:nth-last-of-type(3)'
+        ) as MenuItem;
+        const secondToLastItem = el.querySelector(
+            'sp-menu-item:nth-last-of-type(2)'
+        ) as MenuItem;
+        const arrowUpEvent = new KeyboardEvent('keydown', {
+            bubbles: true,
+            composed: true,
+            code: 'ArrowUp',
+            cancelable: true,
+        });
+        const arrowDownEvent = new KeyboardEvent('keydown', {
+            bubbles: true,
+            composed: true,
+            code: 'ArrowDown',
+            cancelable: true,
+        });
+
+        firstItem.focus();
+
+        expect(activeElement === firstItem).to.be.true;
+
+        el.dispatchEvent(arrowUpEvent);
+        el.dispatchEvent(arrowUpEvent);
+
+        expect(activeElement === thirdToLastItem).to.be.true;
+
+        el.dispatchEvent(arrowDownEvent);
+
+        expect(activeElement === secondToLastItem).to.be.true;
     });
 });
