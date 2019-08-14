d3f9606d70881e985262ff1c17936dde5af247f4
Westbrook <westbrook.johnson@gmail.com>
2019-08-14 18:05:05 -0400

    Start documentation

--- stories/menu.stories.ts
+++ stories/menu.stories.ts
@@ -24,6 +24,28 @@ defineCustomElements(...Object.values(MediumIcons));
 storiesOf('Menu', module)
     .add('Default', () => {
         return html`
+            <sp-menu>
+                <sp-menu-item>
+                    Deselect
+                </sp-menu-item>
+                <sp-menu-item>
+                    Select Inverse
+                </sp-menu-item>
+                <sp-menu-item>
+                    Feather...
+                </sp-menu-item>
+                <sp-menu-item>
+                    Select and Mask...
+                </sp-menu-item>
+                <sp-menu-divider></sp-menu-divider>
+                <sp-menu-item>
+                    Save Selection
+                </sp-menu-item>
+                <sp-menu-item disabled aria-disabled="true">
+                    Make Work Path
+                </sp-menu-item>
+            </sp-menu>
+
             <sp-popover open>
                 <sp-menu>
                     <sp-menu-item>
