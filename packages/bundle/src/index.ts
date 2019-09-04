368eb18ab175d06227d95da909f91fb938f15ea3
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-09-04 08:42:17 -0400

    Add dropdown and action-menu to bundle

--- packages/bundle/src/index.ts
+++ packages/bundle/src/index.ts
@@ -10,7 +10,9 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+export * from '@spectrum-web-components/action-menu';
 export * from '@spectrum-web-components/banner';
 export * from '@spectrum-web-components/button';
+export * from '@spectrum-web-components/dropdown';
 export * from '@spectrum-web-components/icon';
 export * from '@spectrum-web-components/icons';
