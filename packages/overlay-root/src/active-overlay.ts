5bc369294a7455f17b3c608f6d0aa7a9edca76b1
Westbrook Johnson <westbrook.johnson@gmail.com>
2019-09-11 08:10:57 -0400

    Remove debugging code.

--- packages/overlay-root/src/active-overlay.ts
+++ packages/overlay-root/src/active-overlay.ts
@@ -196,10 +196,6 @@ export class ActiveOverlay extends LitElement {
 
     public async hide(): Promise<void> {
         this.state = 'hiding';
-        // if (this.timeout) {
-        //     clearTimeout(this.timeout);
-        //     delete this.timeout;
-        // }
         if (this.hiddenDeferred) {
             return this.hiddenDeferred.promise;
         }
