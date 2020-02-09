2d3141c54d78d8129146d86e33bf1995041194fb
Lars den Bakker <larsdenbakker@gmail.com>
2020-02-09 17:51:17 +0100

    chore: update to @open-wc/demoing-storybook 1.10.1

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -18,50 +18,50 @@ window.process = window.process || {};
 window.process.env = window.process.env || {};
 window.process.env.NODE_ENV = window.process.env.NODE_ENV || 'production';
 
+addDecorator(withA11y);
+addDecorator(withKnobs);
+addDecorator(withWebComponentsKnobs);
+const colorOptions = {
+    Lightest: 'lightest',
+    Light: 'light',
+    Dark: 'dark',
+};
+let defaultColor = colorOptions.Light;
+const scaleOptions = {
+    Medium: 'medium',
+    Large: 'large',
+};
+let defaultScale = scaleOptions.Medium;
+addDecorator((story) => {
+    const color = select('Color', colorOptions, defaultColor, 'Theme');
+    defaultColor = color;
+    const scale = select('Scale', scaleOptions, defaultScale, 'Theme');
+    defaultScale = scale;
+    return html`
+        <sp-theme id="root-theme" color=${color} scale=${scale}>
+            ${story()}
+        </sp-theme>
+    `;
+});
+
+addParameters({
+    a11y: {
+        config: {},
+        options: {
+            checks: { 'color-contrast': { options: { noScroll: true } } },
+            restoreScroll: true,
+        },
+    },
+    docs: {
+        iframeHeight: '200px',
+    },
+});
+
 async function run() {
     const customElements = await (await fetch(
         new URL('../custom-elements.json', import.meta.url)
     )).json();
     setCustomElements(customElements);
-
-    addDecorator(withA11y);
-    addDecorator(withKnobs);
-    addDecorator(withWebComponentsKnobs);
-    const colorOptions = {
-        Lightest: 'lightest',
-        Light: 'light',
-        Dark: 'dark',
-    };
-    let defaultColor = colorOptions.Light;
-    const scaleOptions = {
-        Medium: 'medium',
-        Large: 'large',
-    };
-    let defaultScale = scaleOptions.Medium;
-    addDecorator((story) => {
-        const color = select('Color', colorOptions, defaultColor, 'Theme');
-        defaultColor = color;
-        const scale = select('Scale', scaleOptions, defaultScale, 'Theme');
-        defaultScale = scale;
-        return html`
-            <sp-theme id="root-theme" color=${color} scale=${scale}>
-                ${story()}
-            </sp-theme>
-        `;
-    });
-
-    addParameters({
-        a11y: {
-            config: {},
-            options: {
-                checks: { 'color-contrast': { options: { noScroll: true } } },
-                restoreScroll: true,
-            },
-        },
-        docs: {
-            iframeHeight: '200px',
-        },
-    });
 }
 
 run();
