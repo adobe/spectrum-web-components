7dc74afdda380184906cceadf4eb528b6106792c
Andrew Tran <atran@adobe.com>
2020-04-01 17:02:34 -0700

    fix(slider): dispatch synthetic pointerdown event

--- packages/slider/src/slider.ts
+++ packages/slider/src/slider.ts
@@ -144,9 +144,7 @@ export class Slider extends Focusable {
 
     protected updated(changedProperties: PropertyValues): void {
         if (changedProperties.has('value')) {
-            if (this.dragging) {
-                this.dispatchInputEvent();
-            }
+            this.dispatchInputEvent();
         }
     }
 
@@ -383,6 +381,14 @@ export class Slider extends Focusable {
         this.dragging = true;
         this.handle.setPointerCapture(event.pointerId);
 
+        /**
+         * Dispatch a synthetic pointerdown event to ensure that pointerdown
+         * handlers attached to the slider are invoked before input handlers
+         */
+        event.stopPropagation();
+        const syntheticPointerEvent = new PointerEvent('pointerdown', event);
+        this.dispatchEvent(syntheticPointerEvent);
+
         this.value = this.calculateHandlePosition(event);
     }
 
@@ -440,6 +446,9 @@ export class Slider extends Focusable {
     }
 
     private dispatchInputEvent(): void {
+        if (!this.dragging) {
+            return;
+        }
         const inputEvent = new Event('input', {
             bubbles: true,
             composed: true,
