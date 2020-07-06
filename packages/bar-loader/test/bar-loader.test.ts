b545d92c0c891198e1b5dc38c5014cfd2707e52f
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-07-06 15:59:53 -0400

    feat(bar-loader): add bar-loader pattern

---
+++ packages/bar-loader/test/bar-loader.test.ts
@@ -0,0 +1,96 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { html } from 'lit-element';
+import { fixture, elementUpdated, expect } from '@open-wc/testing';
+
+import '../sp-bar-loader.js';
+import { BarLoader } from '..';
+
+describe('BarLoader', () => {
+    it('loads default bar-loader accessibly', async () => {
+        const el = await fixture<BarLoader>(
+            html`
+                <sp-bar-loader></sp-bar-loader>
+            `
+        );
+
+        await elementUpdated(el);
+        expect(el).to.not.be.undefined;
+        expect(el).lightDom.to.equalSnapshot();
+        expect(el).shadowDom.to.equalSnapshot();
+
+        expect(el).to.be.accessible();
+    });
+
+    it('accepts a changing progress', async () => {
+        const el = await fixture<BarLoader>(html`
+            <sp-bar-loader label="Changing value"></sp-bar-loader>
+        `);
+
+        await elementUpdated(el);
+
+        expect(el.hasAttribute('aria-valuenow')).to.be.true;
+        expect(el.getAttribute('aria-valuenow')).to.equal('0');
+
+        el.value = 50;
+
+        await elementUpdated(el);
+
+        expect(el.hasAttribute('aria-valuenow')).to.be.true;
+        expect(el.getAttribute('aria-valuenow')).to.equal('50');
+
+        el.value = 100;
+
+        await elementUpdated(el);
+
+        expect(el.hasAttribute('aria-valuenow')).to.be.true;
+        expect(el.getAttribute('aria-valuenow')).to.equal('100');
+    });
+    it('loads - [indeterminate]', async () => {
+        const el = await fixture<BarLoader>(html`
+            <sp-bar-loader indeterminate></sp-bar-loader>
+        `);
+
+        await elementUpdated(el);
+        expect(el).to.not.be.undefined;
+        expect(el).lightDom.to.equalSnapshot();
+        expect(el).shadowDom.to.equalSnapshot();
+
+        await expect(el).to.be.accessible();
+    });
+    it('accepts user `role`', async () => {
+        const el = await fixture<BarLoader>(html`
+            <sp-bar-loader role="progressbar"></sp-bar-loader>
+        `);
+
+        await elementUpdated(el);
+
+        expect(el.getAttribute('role')).to.equal('progressbar');
+    });
+    it('returns to indeterminate', async () => {
+        const el = await fixture<BarLoader>(html`
+            <sp-bar-loader value="50"></sp-bar-loader>
+        `);
+
+        await elementUpdated(el);
+
+        expect(el.hasAttribute('aria-valuenow')).to.be.true;
+        expect(el.getAttribute('aria-valuenow')).to.equal('50');
+
+        el.indeterminate = true;
+
+        await elementUpdated(el);
+
+        expect(el.hasAttribute('aria-valuenow')).to.be.false;
+    });
+});
