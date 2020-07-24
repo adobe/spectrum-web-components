af442ea6f525b50943e72371bbac68e65b8df664
Andrew Tran <atran@adobe.com>
2020-07-24 12:20:15 -0700

    feat(accordion): add accordion pattern

---
+++ packages/accordion/src/Accordion.ts
@@ -0,0 +1,169 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    CSSResultArray,
+    TemplateResult,
+    property,
+    PropertyValues,
+} from '@spectrum-web-components/base';
+
+import { AccordionItem } from './AccordionItem.js';
+
+import styles from './accordion.css.js';
+import { Focusable, getActiveElement } from '@spectrum-web-components/shared';
+
+/**
+ * @element sp-accordion
+ */
+export class Accordion extends Focusable {
+    public static get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    public focusedItemIndex = 0;
+    public focusInItemIndex = 0;
+    protected isShiftTabbing = false;
+
+    /**
+     * Allows multiple accordion items to be opened at the same time
+     */
+    @property({ type: Boolean, reflect: true, attribute: 'allow-multiple' })
+    public allowMultiple = false;
+
+    public constructor() {
+        super();
+        this.handleKeydown = this.handleKeydown.bind(this);
+        this.startListeningToKeyboard = this.startListeningToKeyboard.bind(
+            this
+        );
+        this.stopListeningToKeyboard = this.stopListeningToKeyboard.bind(this);
+
+        this.addEventListener('focusin', this.startListeningToKeyboard);
+        this.addEventListener('focusout', this.stopListeningToKeyboard);
+        this.addEventListener('sp-accordion-item-toggle', this.onToggle);
+    }
+
+    public focus(): void {
+        if (this.focusElement.isSameNode(this)) {
+            return;
+        }
+
+        this.focusElement.focus();
+    }
+
+    public get focusElement(): Accordion | AccordionItem {
+        const items = this.querySelectorAll('sp-accordion-item');
+        if (!items.length) {
+            return this;
+        }
+        let index = 0;
+        while (index < items.length && items[index] && items[index].disabled) {
+            index += 1;
+        }
+        /* istanbul ignore else */
+        if (items[index]) {
+            return items[index];
+        }
+        /* istanbul ignore next */
+        return this;
+    }
+
+    public startListeningToKeyboard(): void {
+        const accordionItems = this.querySelectorAll('sp-accordion-item');
+        /* istanbul ignore if */
+        if (accordionItems.length === 0) {
+            return;
+        }
+        this.addEventListener('keydown', this.handleKeydown);
+    }
+
+    public stopListeningToKeyboard(): void {
+        this.removeEventListener('keydown', this.handleKeydown);
+    }
+
+    private handleKeydown(event: KeyboardEvent): void {
+        const { code } = event;
+        /* istanbul ignore if */
+        if (code !== 'ArrowDown' && code !== 'ArrowUp') {
+            return;
+        }
+        event.preventDefault();
+        const direction = code === 'ArrowDown' ? 1 : -1;
+        this.focusItemByOffset(direction);
+    }
+
+    private focusItemByOffset(direction: number): void {
+        const items = [...this.querySelectorAll('sp-accordion-item')];
+        const focused = items.indexOf(getActiveElement(this) as AccordionItem);
+        let next = focused;
+        next = (items.length + next + direction) % items.length;
+        while (items[next].disabled) {
+            next = (items.length + next + direction) % items.length;
+        }
+        items[next].focus();
+    }
+
+    protected manageShiftTab(): void {
+        this.addEventListener('keydown', (event: KeyboardEvent) => {
+            const items = [
+                ...this.querySelectorAll('sp-accordion-item'),
+            ] as AccordionItem[];
+            const firstFocusable = items.find(
+                (item) => !item.disabled
+            ) as AccordionItem;
+            if (
+                !event.defaultPrevented &&
+                event.shiftKey &&
+                event.code === 'Tab' &&
+                (event.composedPath() as AccordionItem[]).includes(
+                    firstFocusable
+                )
+            ) {
+                this.isShiftTabbing = true;
+                HTMLElement.prototype.focus.apply(this);
+                setTimeout(() => (this.isShiftTabbing = false), 0);
+            }
+        });
+    }
+
+    protected render(): TemplateResult {
+        return html`
+            <slot></slot>
+        `;
+    }
+
+    private onToggle(event: Event): void {
+        const target = event.target as AccordionItem;
+        const accordionItems = this.querySelectorAll('sp-accordion-item');
+        /* istanbul ignore if */
+        if (!accordionItems) {
+            return;
+        }
+        if (!this.allowMultiple) {
+            accordionItems.forEach((item: Element) => {
+                const accordionItem = item as AccordionItem;
+                if (accordionItem.open && accordionItem !== target) {
+                    accordionItem.open = false;
+                }
+            });
+        }
+
+        target.open = true;
+    }
+
+    protected firstUpdated(changes: PropertyValues): void {
+        super.firstUpdated(changes);
+        this.tabIndex = 0;
+    }
+}
---
+++ packages/accordion/src/AccordionItem.ts
@@ -0,0 +1,119 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    CSSResultArray,
+    TemplateResult,
+    property,
+    PropertyValues,
+} from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { Focusable } from '@spectrum-web-components/shared/src/focusable.js';
+
+import { ChevronRightMediumIcon } from '@spectrum-web-components/icons-ui';
+
+import styles from './accordion-item.css.js';
+
+/**
+ * @element sp-accordion
+ * @slot - The content of the item that is hidden when the item is not open
+ */
+export class AccordionItem extends Focusable {
+    public static get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public open = false;
+
+    @property({ type: String, reflect: true })
+    public label = '';
+
+    @property({ type: Boolean, reflect: true })
+    public disabled = false;
+
+    public get focusElement(): HTMLElement {
+        /* istanbul ignore if */
+        if (!this.shadowRoot) {
+            return this;
+        }
+        return this.shadowRoot.querySelector('#header') as HTMLElement;
+    }
+
+    constructor() {
+        super();
+
+        this.addEventListener('keydown', this.onKeyDown);
+    }
+
+    private onKeyDown(event: KeyboardEvent): void {
+        if (this.disabled) {
+            return;
+        }
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault();
+            this.dispatchEvent(
+                new CustomEvent('sp-accordion-item-toggle', {
+                    bubbles: true,
+                    composed: true,
+                })
+            );
+        }
+    }
+
+    private onClick(): void {
+        /* istanbul ignore if */
+        if (this.disabled) {
+            return;
+        }
+        this.dispatchEvent(
+            new CustomEvent('sp-accordion-item-toggle', {
+                bubbles: true,
+                composed: true,
+            })
+        );
+    }
+
+    protected render(): TemplateResult {
+        return html`
+            <h3 id="heading">
+                <button
+                    id="header"
+                    @click=${this.onClick}
+                    aria-label=${ifDefined(this.label || undefined)}
+                    aria-expanded=${this.open}
+                    aria-controls="content"
+                >
+                    ${this.label}
+                </button>
+                <sp-icon id="indicator" size="xs">
+                    ${ChevronRightMediumIcon({ hidden: true })}
+                </sp-icon>
+            </h3>
+            <div id="content" role="region" aria-labelledby="header">
+                <slot></slot>
+            </div>
+        `;
+    }
+
+    protected updated(changes: PropertyValues): void {
+        super.updated(changes);
+        if (changes.has('disabled')) {
+            if (this.disabled) {
+                this.setAttribute('aria-disabled', 'true');
+            } else {
+                this.removeAttribute('aria-disabled');
+            }
+        }
+    }
+}
