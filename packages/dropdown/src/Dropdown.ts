4833a598bb3da3552d194586350a3888dba79543
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-07-23 22:24:41 -0400

    feat(split-button): add split-button pattern

--- packages/dropdown/src/Dropdown.ts
+++ packages/dropdown/src/Dropdown.ts
@@ -74,7 +74,11 @@ export class DropdownBase extends Focusable {
     };
 
     @query('#button')
-    public button?: HTMLButtonElement;
+    public button!: HTMLButtonElement;
+
+    public get target(): HTMLButtonElement | this {
+        return this.button;
+    }
 
     @property({ type: Boolean, reflect: true })
     public disabled = false;
@@ -136,10 +140,6 @@ export class DropdownBase extends Focusable {
     }
 
     public get focusElement(): HTMLElement {
-        /* c8 ignore next 3 */
-        if (typeof this.button === 'undefined') {
-            return this;
-        }
         if (this.open && this.optionsMenu) {
             return this.optionsMenu;
         }
@@ -151,11 +151,10 @@ export class DropdownBase extends Focusable {
     }
 
     public onButtonBlur(): void {
-        /* c8 ignore next 3 */
-        if (typeof this.button === 'undefined') {
-            return;
-        }
-        this.button.removeEventListener('keydown', this.onKeydown);
+        (this.target as HTMLButtonElement).removeEventListener(
+            'keydown',
+            this.onKeydown
+        );
     }
 
     protected onButtonClick(): void {
@@ -163,11 +162,10 @@ export class DropdownBase extends Focusable {
     }
 
     public onButtonFocus(): void {
-        /* c8 ignore next 3 */
-        if (typeof this.button === 'undefined') {
-            return;
-        }
-        this.button.addEventListener('keydown', this.onKeydown);
+        (this.target as HTMLButtonElement).addEventListener(
+            'keydown',
+            this.onKeydown
+        );
     }
 
     public onClick(event: Event): void {
@@ -184,7 +182,7 @@ export class DropdownBase extends Focusable {
         }
     }
 
-    public onKeydown(event: KeyboardEvent): void {
+    public onKeydown = (event: KeyboardEvent): void => {
         if (event.code !== 'ArrowDown') {
             return;
         }
@@ -194,7 +192,7 @@ export class DropdownBase extends Focusable {
             return;
         }
         this.open = true;
-    }
+    };
 
     public async setValueFromItem(item: MenuItem): Promise<void> {
         const oldSelectedItemText = this.selectedItemText;
@@ -233,7 +231,7 @@ export class DropdownBase extends Focusable {
         this.open = false;
     }
 
-    private onOverlayClosed(): void {
+    protected onOverlayClosed(): void {
         this.close();
         if (this.optionsMenu && this.placeholder) {
             const parentElement =
@@ -254,7 +252,6 @@ export class DropdownBase extends Focusable {
         /* c8 ignore next 9 */
         if (
             !this.popover ||
-            !this.button ||
             !this.optionsMenu ||
             this.optionsMenu.children.length === 0
         ) {
@@ -274,12 +271,7 @@ export class DropdownBase extends Focusable {
         }
 
         this.popover.append(this.optionsMenu);
-
-        // only use `this.offsetWidth` when Standard variant
-        const menuWidth = !this.quiet && `${this.offsetWidth}px`;
-        if (menuWidth) {
-            this.popover.style.setProperty('width', menuWidth);
-        }
+        this.sizePopover(this.popover);
         const { button, popover } = this;
         this.closeOverlay = await Dropdown.openOverlay(
             button,
@@ -291,6 +283,14 @@ export class DropdownBase extends Focusable {
             }
         );
         this.menuStateResolver();
+    }
+
+    protected sizePopover(popover: HTMLElement): void {
+        // only use `this.offsetWidth` when Standard variant
+        const menuWidth = !this.quiet && `${this.offsetWidth}px`;
+        if (menuWidth) {
+            popover.style.setProperty('width', menuWidth);
+        }
     }
 
     private closeMenu(): void {
@@ -333,6 +333,7 @@ export class DropdownBase extends Focusable {
                 aria-haspopup="true"
                 aria-label=${ifDefined(this.label || undefined)}
                 id="button"
+                class="button"
                 @blur=${this.onButtonBlur}
                 @click=${this.onButtonClick}
                 @focus=${this.onButtonFocus}
@@ -358,27 +359,7 @@ export class DropdownBase extends Focusable {
     protected updated(changedProperties: PropertyValues): void {
         super.updated(changedProperties);
         if (changedProperties.has('value') && this.optionsMenu) {
-            const items = [
-                ...this.optionsMenu.querySelectorAll(
-                    `[role=${this.optionsMenu.childRole}]`
-                ),
-            ] as MenuItem[];
-            let selectedItem: MenuItem | undefined;
-            items.map((item) => {
-                if (this.value === item.value && !item.disabled) {
-                    selectedItem = item;
-                } else {
-                    item.selected = false;
-                }
-            });
-            if (selectedItem) {
-                selectedItem.selected = true;
-                this.selectedItemText = selectedItem.itemText;
-            } else {
-                this.value = '';
-                this.selectedItemText = '';
-            }
-            this.optionsMenu.updateSelectedItemIndex();
+            this.manageSelection();
         }
         if (changedProperties.has('disabled') && this.disabled) {
             this.open = false;
@@ -395,6 +376,35 @@ export class DropdownBase extends Focusable {
             } else {
                 this.closeMenu();
             }
+        }
+    }
+
+    private async manageSelection(): Promise<void> {
+        if (!this.optionsMenu) {
+            return;
+        }
+        if (this.optionsMenu.menuItems.length) {
+            let selectedItem: MenuItem | undefined;
+            this.optionsMenu.menuItems.map((item) => {
+                if (this.value === item.value && !item.disabled) {
+                    selectedItem = item;
+                } else {
+                    item.selected = false;
+                }
+            });
+            if (selectedItem) {
+                selectedItem.selected = true;
+                this.selectedItemText = selectedItem.itemText;
+            } else {
+                this.value = '';
+                this.selectedItemText = '';
+            }
+            this.optionsMenu.updateSelectedItemIndex();
+            return;
+        }
+        await this.optionsMenu.updateComplete;
+        if (this.optionsMenu.menuItems.length) {
+            this.manageSelection();
         }
     }
 
---
+++ packages/split-button/src/SplitButton.ts
@@ -0,0 +1,177 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    CSSResultArray,
+    TemplateResult,
+    property,
+    html,
+    PropertyValues,
+} from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+
+import { ButtonVariants } from '@spectrum-web-components/button';
+import { DropdownBase } from '@spectrum-web-components/dropdown';
+import {
+    ChevronDownMediumIcon,
+    MoreIcon,
+} from '@spectrum-web-components/icons-ui';
+import buttonBaseStyles from '@spectrum-web-components/button/src/button-base.css.js';
+import buttonStyles from '@spectrum-web-components/button/src/button.css.js';
+import ChevronDownMediumStyle from '@spectrum-web-components/icon/src/spectrum-icon-chevron-down-medium.css.js';
+import styles from './split-button.css.js';
+
+/**
+ * @slot options - The menu with options that will display when the dropdown is open
+ */
+export class SplitButton extends DropdownBase {
+    public static get styles(): CSSResultArray {
+        return [buttonBaseStyles, buttonStyles, styles, ChevronDownMediumStyle];
+    }
+
+    /**
+     * The visual variant to apply to this button.
+     */
+    @property({ reflect: true })
+    public variant: ButtonVariants = 'cta';
+
+    public get target(): HTMLButtonElement | this {
+        return this;
+    }
+
+    @property({ type: String })
+    public type: 'field' | 'more' = 'field';
+
+    protected listRole = 'menu';
+    protected itemRole = 'menuitem';
+
+    protected sizePopover(popover: HTMLElement): void {
+        popover.style.setProperty('min-width', `${this.offsetWidth}px`);
+    }
+
+    private passClick(): void {
+        /* istanbul ignore if */
+        if (!this.optionsMenu) {
+            return;
+        }
+        const target =
+            this.optionsMenu.menuItems.find((el) => el.selected) ||
+            this.optionsMenu.menuItems[0];
+        /* istanbul ignore else */
+        if (target) {
+            target.click();
+        }
+    }
+
+    protected manageShiftTab(): void {
+        this.addEventListener('keydown', (event) => {
+            const target = event.composedPath()[0];
+            if (
+                target &&
+                target === this.focusElement &&
+                !event.defaultPrevented &&
+                event.shiftKey &&
+                event.code === 'Tab'
+            ) {
+                this.isShiftTabbing = true;
+                HTMLElement.prototype.focus.apply(this);
+                setTimeout(() => (this.isShiftTabbing = false), 0);
+            }
+        });
+    }
+
+    protected get buttonContent(): TemplateResult[] {
+        return [
+            html`
+                <div
+                    id="label"
+                    class=${ifDefined(this.value ? undefined : 'placeholder')}
+                >
+                    ${this.selectedItemText}
+                </div>
+            `,
+        ];
+    }
+
+    protected render(): TemplateResult {
+        return html`
+            <button
+                aria-haspopup="true"
+                aria-label=${ifDefined(this.label || undefined)}
+                id="button"
+                class="button ${this.variant}"
+                @click=${this.passClick}
+                ?disabled=${this.disabled}
+            >
+                ${this.buttonContent}
+            </button>
+            <button
+                class="button trigger ${this.variant}"
+                @blur=${this.onButtonBlur}
+                @click=${this.onButtonClick}
+                @focus=${this.onButtonFocus}
+                ?disabled=${this.disabled}
+            >
+                <sp-icon
+                    class="icon ${this.type === 'field'
+                        ? 'chevron-down-medium'
+                        : 'more-medium'}"
+                >
+                    ${this.type === 'field'
+                        ? ChevronDownMediumIcon({ hidden: true })
+                        : MoreIcon({ hidden: true })}
+                </sp-icon>
+            </button>
+            <sp-popover
+                open
+                id="popover"
+                @click=${this.onClick}
+                @sp-overlay-closed=${this.onOverlayClosed}
+            ></sp-popover>
+        `;
+    }
+
+    protected updated(changedProperties: PropertyValues): void {
+        super.updated(changedProperties);
+        if (changedProperties.has('value')) {
+            this.manageSplitButtonItems();
+        }
+    }
+
+    private async manageSplitButtonItems(): Promise<void> {
+        /* istanbul ignore if */
+        if (!this.optionsMenu) {
+            return;
+        }
+        if (this.optionsMenu.menuItems.length) {
+            if (this.type === 'more') {
+                this.optionsMenu.menuItems[0].hidden = true;
+                this.optionsMenu.menuItems.forEach(
+                    (el) => (el.selected = false)
+                );
+                this.selectedItemText = this.optionsMenu.menuItems[0].itemText;
+            } else {
+                const selected =
+                    this.optionsMenu.menuItems.find((el) => el.selected) ||
+                    this.optionsMenu.menuItems[0];
+                selected.selected = true;
+                this.selectedItemText = selected.itemText;
+            }
+            return;
+        }
+        await this.optionsMenu.updateComplete;
+        /* istanbul ignore else */
+        if (this.optionsMenu.menuItems.length) {
+            this.manageSplitButtonItems();
+        }
+    }
+}
