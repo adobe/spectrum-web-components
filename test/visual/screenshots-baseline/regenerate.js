a3d99a4fad7c5d6817c835d66e5c6601e86e878b
Westbrook Johnson <wesjohns@adobe.com>
2020-08-14 17:27:58 -0400

    ci: expand visual regressions to RTL

--- test/visual/screenshots-baseline/regenerate.js
+++ test/visual/screenshots-baseline/regenerate.js
@@ -18,7 +18,7 @@ const baselineDir = `${process.cwd()}/test/visual/screenshots-baseline`;
 const stories = require('../stories');
 
 module.exports = {
-    buildScreenshots(type, color = 'light', scale = 'medium') {
+    buildScreenshots(type, color = 'light', scale = 'medium', dir = 'ltr') {
         describe('🎁 regenerate screenshots', function () {
             let server,
                 browser,
@@ -76,10 +76,9 @@ module.exports = {
         async function generateBaselineScreenshots(page) {
             const prefix = type;
             console.log(prefix + '...');
-
             for (let i = 0; i < stories.length; i++) {
                 await page.goto(
-                    `http://127.0.0.1:4444/iframe.html?id=${stories[i]}&knob-Color_Theme=${color}&knob-Scale_Theme=${scale}`,
+                    `http://127.0.0.1:4444/iframe.html?id=${stories[i]}&knob-Color_Theme=${color}&knob-Scale_Theme=${scale}&knob-Text direction_Theme=${dir}`,
                     {
                         waitUntil: 'networkidle',
                     }
@@ -88,7 +87,7 @@ module.exports = {
                     '!!document.querySelector("sp-theme").shadowRoot'
                 );
                 await page.screenshot({
-                    path: `${baselineDir}/${type}/${stories[i]}__${color}__${scale}.png`,
+                    path: `${baselineDir}/${type}/${stories[i]}__${color}__${scale}__${dir}.png`,
                 });
             }
         }
