f12168fedcd850ff5c72587a74614fdef42b6b2f
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-08-18 08:16:57 -0400

    fix(tabs): bind tabindicator update to dir value

--- packages/tabs/src/Tabs.ts
+++ packages/tabs/src/Tabs.ts
@@ -131,6 +131,9 @@ export class Tabs extends Focusable {
                 this.setAttribute('aria-orientation', 'vertical');
             }
         }
+        if (changes.has('dir')) {
+            this.updateSelectionIndicator();
+        }
     }
 
     /**
