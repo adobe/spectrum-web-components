dc94624786d9e1d1b056e664f64e2e36c49a7d68
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-10-22 10:38:43 -0400

    test: compare local and latest builds of a component

--- packages/slider/test/benchmark/test-basic.ts
+++ packages/slider/test/benchmark/test-basic.ts
@@ -12,7 +12,7 @@ governing permissions and limitations under the License.
 
 import '@spectrum-web-components/slider/sp-slider.js';
 import { html } from '@spectrum-web-components/base';
-import { measureFixtureCreation } from '../../../../test/benchmark/helpers';
+import { measureFixtureCreation } from '../../../../test/benchmark/helpers.js';
 
 measureFixtureCreation(html`
     <sp-slider
