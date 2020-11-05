f5c9e6910a20850d982e5083846348fb68bdd7ac
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-11-05 09:09:01 -0500

    test: use latest playwright to run firefox tests concurrently

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,3 +1,14 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 const { playwrightLauncher } = require('@web/test-runner-playwright');
 
 module.exports = {
@@ -18,7 +29,7 @@ module.exports = {
         ],
         threshold: {
             statements: 98,
-            branches: 94,
+            branches: 93,
             functions: 94,
             lines: 98,
         },
@@ -28,24 +39,10 @@ module.exports = {
             timeout: 10000,
         },
     },
-    testRunnerHtml: (testRunnerImport) => `
-        <html>
-        <head></head>
-        <body>
-            <script type="module">
-            import '${testRunnerImport}';
-            window.process = window.process || {};
-            window.process.env = window.process.env || {};
-            window.process.env.NODE_ENV = window.process.env.NODE_ENV || 'production';
-            </script>
-        </body>
-        </html>
-    `,
     browsers: [
         playwrightLauncher({ product: 'chromium' }),
         playwrightLauncher({ product: 'webkit' }),
         playwrightLauncher({
-            concurrency: 1,
             product: 'firefox',
             launchOptions: {
                 headless: false,
