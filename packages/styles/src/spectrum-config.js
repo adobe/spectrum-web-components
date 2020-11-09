1658efac81f72237759fbc6b2f7bdb19ebe1dae6
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-11-09 10:18:01 -0500

    feat(styles): vend CSS literal versions of the typography system

---
+++ packages/styles/src/spectrum-config.js
@@ -0,0 +1,69 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const config = {
+    spectrum: 'typography',
+    components: [
+        {
+            name: 'base',
+            host: {
+                selector: '.spectrum',
+                shadowSelector: '.spectrum',
+            },
+            exclude: [/\.spectrum-/, /\.spectrum:lang/],
+        },
+        {
+            name: 'typography',
+            host: {
+                selector: '.spectrum-Typography',
+                shadowSelector: '.spectrum-Typography',
+            },
+        },
+        {
+            name: 'heading',
+            host: {
+                selector: '.spectrum-Heading',
+                shadowSelector: '.spectrum-Heading',
+            },
+        },
+        {
+            name: 'body',
+            host: {
+                selector: '.spectrum-Body',
+                shadowSelector: '.spectrum-Body',
+            },
+        },
+        {
+            name: 'detail',
+            host: {
+                selector: '.spectrum-Detail',
+                shadowSelector: '.spectrum-Detail',
+            },
+        },
+        {
+            name: 'code',
+            host: {
+                selector: '.spectrum-Code',
+                shadowSelector: '.spectrum-Code',
+            },
+        },
+        {
+            name: 'lang',
+            host: {
+                selector: '.spectrum',
+                shadowSelector: '.spectrum',
+            },
+            exclude: [/^\.spectrum(?!:lang)/],
+        },
+    ],
+};
+
+export default config;
