fa092ba915a2fe6320cd9bdbe33055a9e41eee87
Najika Yoo <halsema@najikas-mbp.sea.adobe.com>
2020-11-13 12:49:44 -0800

    feat(meter): add meter pattern

---
+++ packages/meter/src/spectrum-config.js
@@ -0,0 +1,78 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const config = {
+    spectrum: 'progressbar',
+    components: [
+        {
+            name: 'meter',
+            host: {
+                selector: '.spectrum-ProgressBar',
+            },
+            classes: [
+                {
+                    selector: '.spectrum-ProgressBar-track',
+                    name: 'track',
+                },
+                {
+                    selector: '.spectrum-ProgressBar-fill',
+                    name: 'fill',
+                },
+                {
+                    selector: '.spectrum-ProgressBar-label',
+                    name: 'label',
+                },
+                {
+                    selector: '.spectrum-ProgressBar-percentage',
+                    name: 'percentage',
+                },
+            ],
+            attributes: [
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-ProgressBar--sideLabel',
+                    name: 'side-label',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-ProgressBar--small',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-ProgressBar--indeterminate',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.spectrum-ProgressBar--overBackground',
+                    name: 'over-background',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-positive',
+                    name: 'positive',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-warning',
+                    name: 'warning',
+                },
+                {
+                    type: 'boolean',
+                    selector: '.is-critical',
+                    name: 'critical',
+                },
+            ],
+        },
+    ],
+};
+
+export default config;
---
+++ packages/meter/stories/meter.stories.ts
@@ -0,0 +1,72 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { html, TemplateResult } from '@spectrum-web-components/base';
+
+import '../sp-meter.js';
+
+export default {
+    title: 'Meter',
+    component: 'sp-meter',
+};
+
+const makeOverBackground = (story: TemplateResult): TemplateResult => html`
+    <div
+        style="background-color: var(--spectrum-global-color-seafoam-600); color: var(--spectrum-global-color-seafoam-600); padding: var(--spectrum-global-dimension-size-175) var(--spectrum-global-dimension-size-250); display: inline-block"
+    >
+        ${story}
+    </div>
+`;
+
+export const Default = (): TemplateResult => {
+    return html`
+        <sp-meter progress="50">Storage Space</sp-meter>
+    `;
+};
+
+export const sideLabel = (): TemplateResult => {
+    return html`
+        <sp-meter side-label progress="50">Storage Space</sp-meter>
+    `;
+};
+
+export const small = (): TemplateResult => {
+    return html`
+        <sp-meter small progress="50">Storage Space</sp-meter>
+    `;
+};
+
+export const critical = (): TemplateResult => {
+    return html`
+        <sp-meter critical progress="95">Storage Space</sp-meter>
+    `;
+};
+
+export const warning = (): TemplateResult => {
+    return html`
+        <sp-meter warning progress="73">Storage Space</sp-meter>
+    `;
+};
+
+export const positive = (): TemplateResult => {
+    return html`
+        <sp-meter positive progress="50">Storage Space</sp-meter>
+    `;
+};
+
+export const overBackground = (): TemplateResult => {
+    return makeOverBackground(
+        html`
+            <sp-meter over-background progress="50">Storage Space</sp-meter>
+        `
+    );
+};
