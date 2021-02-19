1b4ff2d345fe5d64c7a2b5e2ac817260ef0ba6e4
Najika Yoo <halsema@najikas-mbp.sea.adobe.com>
2021-02-19 14:07:38 -0800

    feat(color-loupe): add color-loupe pattern

---
+++ packages/color-loupe/src/spectrum-config.js
@@ -0,0 +1,42 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const config = {
+    spectrum: 'colorloupe',
+    components: [
+        {
+            name: 'color-loupe',
+            host: {
+                selector: '.spectrum-ColorLoupe',
+            },
+            attributes: [
+                {
+                    type: 'boolean',
+                    name: 'open',
+                    selector: '.is-open',
+                },
+            ],
+            classes: [
+                {
+                    selector: '.spectrum-ColorLoupe-outer',
+                    name: 'outer',
+                },
+                {
+                    selector: '.spectrum-ColorLoupe-inner',
+                    name: 'inner',
+                },
+            ],
+        },
+    ],
+};
+
+export default config;
