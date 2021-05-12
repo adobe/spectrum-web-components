0d6e1d4c3dce08dc16a619f41e7ecbf3c1422936
CircleCI <circleci@adobe.com>
2021-05-12 17:50:00 +0000

    [skip ci] update demonstration site

---
+++ precache-manifest.7ff971dbc544f82e365946417e463f9f.js
@@ -0,0 +1,103 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "0.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "0.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "1.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "1.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "10.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "10.00a97449675c320fcc73.js.LICENSE.txt"
+  },
+  {
+    "url": "10.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "11.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "11.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "3.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "3.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "4.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "4.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "5.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "5.00a97449675c320fcc73.js.LICENSE.txt"
+  },
+  {
+    "url": "5.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "6.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "6.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "7.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "7.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "8.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "8.00a97449675c320fcc73.js.LICENSE.txt"
+  },
+  {
+    "url": "8.00a97449675c320fcc73.js.map"
+  },
+  {
+    "url": "9.00a97449675c320fcc73.js"
+  },
+  {
+    "url": "9.00a97449675c320fcc73.js.map"
+  },
+  {
+    "revision": "b790c7ebb922a430223c6a2e5b2a7fe6",
+    "url": "index.html"
+  },
+  {
+    "url": "main.00a97449675c320fcc73.bundle.js"
+  },
+  {
+    "url": "main.00a97449675c320fcc73.bundle.js.map"
+  },
+  {
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0",
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js"
+  },
+  {
+    "revision": "8b83f2a1a44585fbda36730266d3889a",
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js.map"
+  },
+  {
+    "revision": "939ef49522c9d6762c23f0a3fb10e87b",
+    "url": "polyfills/webcomponents.f2956532e796a677ef87a9cb17f5f9fc.js"
+  },
+  {
+    "revision": "e28182d5a543b663959f5f3d8ba9ed06",
+    "url": "polyfills/webcomponents.f2956532e796a677ef87a9cb17f5f9fc.js.map"
+  }
+]);
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.67867a1936fc9d4f3aafc9bfb0e7f812.js"
+  "precache-manifest.7ff971dbc544f82e365946417e463f9f.js"
 );
 
 self.addEventListener('message', (event) => {
