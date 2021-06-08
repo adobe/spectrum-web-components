edcba81ed80cd0a1a0a784df1438ac8220d3db0c
Westbrook Johnson <westbrook.johnson@gmail.com>
2021-06-08 11:41:47 -0400

    chore: use pre-release @web/dev-server-storybook for easier upgrade to lit@2.0

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,8 @@
-import { addDecorator } from '@web/storybook-prebuilt/web-components.js';
-import { swcThemeDecoratorWithConfig } from '@spectrum-web-components/story-decorator/decorator.js';
+import { swcThemeDecorator } from '@spectrum-web-components/story-decorator/decorator.js';
 
 export const parameters = {
     controls: { expanded: true },
     layout: 'fullscreen',
 };
 
-addDecorator(swcThemeDecoratorWithConfig({ bundled: false }));
+export const decorators = [swcThemeDecorator];
