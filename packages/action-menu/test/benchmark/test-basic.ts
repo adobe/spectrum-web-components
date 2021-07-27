8cfac32a45fdceb873d18fbd9fbff2ba3363e1a6
Westbrook Johnson <westbrook.johnson@gmail.com>
2021-07-27 14:02:13 -0400

    test: update Action Menu tests for current API

--- packages/action-menu/test/benchmark/test-basic.ts
+++ packages/action-menu/test/benchmark/test-basic.ts
@@ -18,27 +18,15 @@ import { measureFixtureCreation } from '../../../../test/benchmark/helpers.js';
 
 measureFixtureCreation(html`
     <sp-action-menu>
-        Select a Country with a very long label, too long in fact
-        <sp-menu slot="options">
-            <sp-menu-item>
-                Deselect
-            </sp-menu-item>
-            <sp-menu-item>
-                Select Inverse
-            </sp-menu-item>
-            <sp-menu-item>
-                Feather...
-            </sp-menu-item>
-            <sp-menu-item>
-                Select and Mask...
-            </sp-menu-item>
-            <sp-menu-divider></sp-menu-divider>
-            <sp-menu-item>
-                Save Selection
-            </sp-menu-item>
-            <sp-menu-item disabled>
-                Make Work Path
-            </sp-menu-item>
-        </sp-menu>
+        <span slot="label">
+            Select a Country with a very long label, too long in fact
+        </span>
+        <sp-menu-item>Deselect</sp-menu-item>
+        <sp-menu-item>Select Inverse</sp-menu-item>
+        <sp-menu-item>Feather...</sp-menu-item>
+        <sp-menu-item>Select and Mask...</sp-menu-item>
+        <sp-menu-divider></sp-menu-divider>
+        <sp-menu-item>Save Selection</sp-menu-item>
+        <sp-menu-item disabled>Make Work Path</sp-menu-item>
     </sp-action-menu>
 `);
