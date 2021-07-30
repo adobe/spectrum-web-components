ecdc021a3824ee476fb708e340d58b51cd665175
Westbrook <westbrook.johnson@gmail.com>
2021-07-30 14:08:34 -0400

    ci: use higher concurrency

--- web-test-runner.config.vrt.js
+++ web-test-runner.config.vrt.js
@@ -11,7 +11,7 @@ governing permissions and limitations under the License.
 */
 import standard from './web-test-runner.config.ci.js';
 
-standard.concurrency = 2;
+standard.concurrency = 4;
 standard.testsFinishTimeout = 200000;
 standard.testFramework.config.timeout = 100000;
 
