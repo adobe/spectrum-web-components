4e7c6df9d0d82e6bb10a63c69889b00b61f252ef
CircleCI <circleci@adobe.com>
2021-09-07 20:52:40 +0000

    [skip ci] update demonstration site

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8417e9c87ddb2c6644a5280fdd1977be.js"
+  "precache-manifest.09b9ac7f1e13822e42b8dead15ae627c.js"
 );
 
 self.addEventListener('message', (event) => {
