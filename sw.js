4f4a89d90822eca1322ad865def07fc372a0867c
Westbrook Johnson <westbrook.johnson@gmail.com>
2021-09-23 08:14:11 -0400

    [skip ci] update demonstration site

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.dbf154a32f83b89243eb1163bb7e29b8.js"
+  "precache-manifest.b5abf18afd9882a24b356763d67e84ae.js"
 );
 
 self.addEventListener('message', (event) => {
