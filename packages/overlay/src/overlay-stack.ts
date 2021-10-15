c5864efe2ccfb48d65281bee152ae9cdea836419
Westbrook <westbrook.johnson@gmail.com>
2021-10-15 13:59:11 -0500

    ci: update golden image cache

--- packages/overlay/src/overlay-stack.ts
+++ packages/overlay/src/overlay-stack.ts
@@ -391,6 +391,54 @@ export class OverlayStack {
         }
     }
 
+    private manageFocusAfterCloseWhenOverlaysRemain(): void {
+        const topOverlay = this.overlays[this.overlays.length - 1];
+        topOverlay.feature();
+        // Push focus in the the next remaining overlay as needed when a `type="modal"` overlay exists.
+        if (topOverlay.interaction === 'modal' || topOverlay.hasModalRoot) {
+            topOverlay.focus();
+        } else {
+            this.stopTabTrapping();
+        }
+    }
+
+    private manageFocusAfterCloseWhenLastOverlay(overlay: ActiveOverlay): void {
+        this.stopTabTrapping();
+        const isModal = overlay.interaction === 'modal';
+        const isReplace = overlay.interaction === 'replace';
+        const isInline = overlay.interaction === 'inline';
+        const isTabbingAwayFromInlineOrReplace =
+            (isReplace || isInline) && !overlay.tabbingAway;
+        overlay.tabbingAway = false;
+        if (!isModal && !isTabbingAwayFromInlineOrReplace) {
+            return;
+        }
+        // Manage post closure focus when needed.
+        const overlayRoot = overlay.overlayContent.getRootNode() as ShadowRoot;
+        const overlayContentActiveElement = overlayRoot.activeElement;
+        let triggerRoot: ShadowRoot;
+        let triggerActiveElement: Element | null;
+        const contentContainsActiveElement = (): boolean =>
+            overlay.overlayContent.contains(overlayContentActiveElement);
+        const triggerRootContainsActiveElement = (): boolean => {
+            triggerRoot = overlay.trigger.getRootNode() as ShadowRoot;
+            triggerActiveElement = triggerRoot.activeElement;
+            return triggerRoot.contains(triggerActiveElement);
+        };
+        const triggerHostIsActiveElement = (): boolean =>
+            triggerRoot.host && triggerRoot.host === triggerActiveElement;
+        // Return focus to the trigger as long as the user hasn't actively focused
+        // something outside of the current overlay interface; trigger, root, host.
+        if (
+            isModal ||
+            contentContainsActiveElement() ||
+            triggerRootContainsActiveElement() ||
+            triggerHostIsActiveElement()
+        ) {
+            overlay.trigger.focus();
+        }
+    }
+
     private async hideAndCloseOverlay(
         overlay?: ActiveOverlay,
         animated?: boolean
@@ -408,48 +456,18 @@ export class OverlayStack {
             const { trigger } = overlay;
             contentWithLifecycle.overlayCloseCallback({ trigger });
         }
+
         if (overlay.state != 'dispose') return;
 
         const index = this.overlays.indexOf(overlay);
         if (index >= 0) {
             this.overlays.splice(index, 1);
         }
+
         if (this.overlays.length) {
-            const topOverlay = this.overlays[this.overlays.length - 1];
-            topOverlay.feature();
-            if (topOverlay.interaction === 'modal' || topOverlay.hasModalRoot) {
-                topOverlay.focus();
-            } else {
-                this.stopTabTrapping();
-            }
+            this.manageFocusAfterCloseWhenOverlaysRemain();
         } else {
-            this.stopTabTrapping();
-            if (
-                overlay.interaction === 'modal' ||
-                ((overlay.interaction === 'replace' ||
-                    overlay.interaction === 'inline') &&
-                    !overlay.tabbingAway)
-            ) {
-                const overlayRoot =
-                    overlay.overlayContent.getRootNode() as ShadowRoot;
-                const overlayContentActiveElement = overlayRoot.activeElement;
-                const triggerRoot = overlay.trigger.getRootNode() as ShadowRoot;
-                const triggerActiveElement = triggerRoot.activeElement;
-                if (
-                    overlay.interaction === 'modal' ||
-                    overlay.overlayContent.contains(
-                        overlayContentActiveElement
-                    ) ||
-                    overlay.trigger
-                        .getRootNode()
-                        .contains(triggerActiveElement) ||
-                    (triggerRoot.host &&
-                        triggerRoot.host === triggerActiveElement)
-                ) {
-                    overlay.trigger.focus();
-                }
-            }
-            overlay.tabbingAway = false;
+            this.manageFocusAfterCloseWhenLastOverlay(overlay);
         }
 
         overlay.remove();
