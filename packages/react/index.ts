32956d524f2aff07667425ae23fca88bd60e3ad4
luvsic3 <1021679588@qq.com>
2021-10-28 16:57:59 +0800

    chore: copyright

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,76 +1,88 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+Copyright 2021 Gaoding. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 /** 注册 web components sideEffect */
-import '@iliad-ui/bundle/elements'
+import '@iliad-ui/bundle/elements';
 
 /** 脚本自动生成 */
-export * from "./src/Accordion";
-export * from "./src/AccordionItem";
-export * from "./src/ActionButton";
-export * from "./src/ActionBar";
-export * from "./src/ActionGroup";
-export * from "./src/ActionMenu";
-export * from "./src/Avatar";
-export * from "./src/Asset";
-export * from "./src/Banner";
-export * from "./src/Breadcrumb";
-export * from "./src/Button";
-export * from "./src/ClearButton";
-export * from "./src/ButtonGroup";
-export * from "./src/Card";
-export * from "./src/Coachmark";
-export * from "./src/Checkbox";
-export * from "./src/ColorLoupe";
-export * from "./src/ColorArea";
-export * from "./src/ColorHandle";
-export * from "./src/ColorSlider";
-export * from "./src/ColorWheel";
-export * from "./src/Dialog";
-export * from "./src/DialogWrapper";
-export * from "./src/Divider";
-export * from "./src/Dropzone";
-export * from "./src/FieldGroup";
-export * from "./src/FieldLabel";
-export * from "./src/Icon";
-export * from "./src/Link";
-export * from "./src/IllustratedMessage";
-export * from "./src/Meter";
-export * from "./src/NumberField";
-export * from "./src/PickerBase";
-export * from "./src/Popover";
-export * from "./src/ProgressBar";
-export * from "./src/ProgressCircle";
-export * from "./src/Menu";
-export * from "./src/MenuDivider";
-export * from "./src/MenuGroup";
-export * from "./src/MenuItem";
-export * from "./src/QuickActions";
-export * from "./src/ActiveOverlay";
-export * from "./src/OverlayTrigger";
-export * from "./src/Search";
-export * from "./src/Radio";
-export * from "./src/RadioGroup";
-export * from "./src/SideNav";
-export * from "./src/SideNavHeading";
-export * from "./src/SideNavItem";
-export * from "./src/Slider";
-export * from "./src/SliderHandle";
-export * from "./src/SplitView";
-export * from "./src/StatusLight";
-export * from "./src/Tab";
-export * from "./src/TabPanel";
-export * from "./src/Tabs";
-export * from "./src/Switch";
-export * from "./src/Tag";
-export * from "./src/Tags";
-export * from "./src/Textfield";
-export * from "./src/Theme";
-export * from "./src/Thumbnail";
-export * from "./src/Toast";
-export * from "./src/Tooltip";
-export * from "./src/TopNav";
-export * from "./src/TopNavItem";
-export * from "./src/Tray";
-export * from "./src/Tree";
-export * from "./src/Underlay";
+export * from './src/Accordion';
+export * from './src/AccordionItem';
+export * from './src/ActionButton';
+export * from './src/ActionBar';
+export * from './src/ActionGroup';
+export * from './src/ActionMenu';
+export * from './src/Avatar';
+export * from './src/Asset';
+export * from './src/Banner';
+export * from './src/Breadcrumb';
+export * from './src/Button';
+export * from './src/ClearButton';
+export * from './src/ButtonGroup';
+export * from './src/Card';
+export * from './src/Coachmark';
+export * from './src/Checkbox';
+export * from './src/ColorLoupe';
+export * from './src/ColorArea';
+export * from './src/ColorHandle';
+export * from './src/ColorSlider';
+export * from './src/ColorWheel';
+export * from './src/Dialog';
+export * from './src/DialogWrapper';
+export * from './src/Divider';
+export * from './src/Dropzone';
+export * from './src/FieldGroup';
+export * from './src/FieldLabel';
+export * from './src/Icon';
+export * from './src/Link';
+export * from './src/IllustratedMessage';
+export * from './src/Meter';
+export * from './src/NumberField';
+export * from './src/PickerBase';
+export * from './src/Popover';
+export * from './src/ProgressBar';
+export * from './src/ProgressCircle';
+export * from './src/Menu';
+export * from './src/MenuDivider';
+export * from './src/MenuGroup';
+export * from './src/MenuItem';
+export * from './src/QuickActions';
+export * from './src/ActiveOverlay';
+export * from './src/OverlayTrigger';
+export * from './src/Search';
+export * from './src/Radio';
+export * from './src/RadioGroup';
+export * from './src/SideNav';
+export * from './src/SideNavHeading';
+export * from './src/SideNavItem';
+export * from './src/Slider';
+export * from './src/SliderHandle';
+export * from './src/SplitView';
+export * from './src/StatusLight';
+export * from './src/Tab';
+export * from './src/TabPanel';
+export * from './src/Tabs';
+export * from './src/Switch';
+export * from './src/Tag';
+export * from './src/Tags';
+export * from './src/Textfield';
+export * from './src/Theme';
+export * from './src/Thumbnail';
+export * from './src/Toast';
+export * from './src/Tooltip';
+export * from './src/TopNav';
+export * from './src/TopNavItem';
+export * from './src/Tray';
+export * from './src/Tree';
+export * from './src/Underlay';
 
 /** 手动添加 */
-export * from "./src/Picker";
+export * from './src/Picker';
