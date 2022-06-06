d34dbe04f6b763ff20793da059dd767e9bf00fa7
Westbrook Johnson <wesjohns@adobe.com>
2022-06-06 12:25:31 -0400

    chore: catch up the Rollup Example

--- projects/example-project-rollup/rollup.config.js
+++ projects/example-project-rollup/rollup.config.js
@@ -1,3 +1,15 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
 import merge from 'deepmerge';
 // use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
@@ -20,6 +32,10 @@ const baseConfig = createSpaConfig({
 
   // set to true to inject the service worker registration into your index.html
   injectServiceWorker: false,
+
+  nodeResolve: {
+    exportConditions: ['browser', 'production'],
+  },
 });
 
 export default merge(baseConfig, {
