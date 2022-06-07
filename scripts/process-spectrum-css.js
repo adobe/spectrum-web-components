40a814bbeec9033f6fb710aff87fe3c7803ad1cb
Westbrook Johnson <wesjohns@adobe.com>
2022-06-07 14:46:59 -0400

    feat: support Spectrum Token consumption and update Action Button to use them

--- scripts/process-spectrum-css.js
+++ scripts/process-spectrum-css.js
@@ -52,14 +52,14 @@ async function processConfig(spectrumConfig, componentPath) {
         process.exit(1);
     }
     const inputCss = await fs.readFile(inputCssPath);
-    let inputCustomProperties = await fs.readFile(
-        `node_modules/@spectrum-css/${spectrumConfig.spectrum}/dist/vars.css`,
-        'utf8'
-    );
-    inputCustomProperties = inputCustomProperties.replace(
-        /(.|\n)*\{/,
-        ':root {'
-    );
+    // let inputCustomProperties = await fs.readFile(
+    //     `node_modules/@spectrum-css/${spectrumConfig.spectrum}/dist/vars.css`,
+    //     'utf8'
+    // );
+    // inputCustomProperties = inputCustomProperties.replace(
+    //     /(.|\n)*\{/,
+    //     ':root {'
+    // );
     // eslint-disable-next-line no-console
     console.log(chalk.bold.green(`- ${spectrumConfig.spectrum}`));
     return Promise.all(
@@ -80,14 +80,16 @@ async function processConfig(spectrumConfig, componentPath) {
                 from: inputCssPath,
                 to: outputCssPath,
             });
-            const srcPath = `node_modules/@spectrum-css/${spectrumConfig.spectrum}/dist/vars.css`;
-            await postcss([
-                postcssCustomProperties({
-                    exportTo: [outputJsonPath],
-                }),
-            ]).process(inputCustomProperties, {
-                from: srcPath,
-            });
+            // const srcPath = `node_modules/@spectrum-css/${spectrumConfig.spectrum}/dist/vars.css`;
+            // if (fs.existsSync(srcPath)) {
+            //     await postcss([
+            //         postcssCustomProperties({
+            //             exportTo: [outputJsonPath],
+            //         }),
+            //     ]).process(inputCustomProperties, {
+            //         from: srcPath,
+            //     });
+            // }
             // eslint-disable-next-line no-console
             console.log(chalk.bold.green(`  o ${component.name}`));
 
@@ -116,6 +118,7 @@ async function processComponents() {
 
 async function main() {
     await processComponents();
+    process.exit(0);
 }
 
 main();
