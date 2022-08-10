6e780ba55be56a1c0fb2ffc6db38f3be206f7c09
Westbrook Johnson <wesjohns@adobe.com>
2022-08-10 16:10:14 -0400

    test: update retries to better support VRT and flaky testing

--- web-test-runner.config.vrt.js
+++ web-test-runner.config.vrt.js
@@ -14,5 +14,6 @@ import standard from './web-test-runner.config.ci.js';
 standard.concurrency = 4;
 standard.testsFinishTimeout = 200000;
 standard.testFramework.config.timeout = 100000;
+standard.testFramework.config.retries = 4;
 
 export default standard;
