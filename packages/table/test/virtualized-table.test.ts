2ef04c222a051993c8d7cde3a9d7248e02759151
Najika Yoo <halsema@najikas-mbp.sea.adobe.com>
2022-08-11 12:33:29 -0700

    test(table): fix a11y error by adding tab stop on tablebody

--- packages/table/test/virtualized-table.test.ts
+++ packages/table/test/virtualized-table.test.ts
@@ -59,6 +59,9 @@ describe('Virtualized Table', () => {
 
     it('loads virtualized table accessibly', async () => {
         const el = await fixture<Table>(virtualized());
+        await nextFrame();
+        await nextFrame();
+        await nextFrame();
         await expect(el).to.be.accessible();
     });
 
