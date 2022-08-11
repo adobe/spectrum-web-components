916e0d31e3f600db588897fa7bb9f3fdbebaa338
Najika Yoo <halsema@najikas-mbp.sea.adobe.com>
2022-08-11 12:33:29 -0700

    test(table): fix a11y error by adding tab stop on tablebody

--- packages/table/src/Table.ts
+++ packages/table/src/Table.ts
@@ -453,6 +453,7 @@ export class Table extends SizedMixin(SpectrumElement, {
             this.tableBody = this.querySelector('sp-table-body') as TableBody;
             if (!this.tableBody) {
                 this.tableBody = document.createElement('sp-table-body');
+                this.tableBody.tabIndex = 0;
                 this.append(this.tableBody);
             }
             this.tableBody.addEventListener(
