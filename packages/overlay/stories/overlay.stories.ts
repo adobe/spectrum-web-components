161821953d79d7aae194ed83a2b727d7e22eb10e
Hunter Loftis <hunter@hunterloftis.com>
2022-10-05 13:30:07 -0400

    chore: add story reproducing flicker

--- packages/overlay/stories/overlay.stories.ts
+++ packages/overlay/stories/overlay.stories.ts
@@ -961,3 +961,23 @@ export const definedOverlayElement = (): TemplateResult => {
 };
 
 definedOverlayElement.decorators = [definedOverlayDecorator];
+
+export const modalWithinNonModal = (): TemplateResult => {
+    return html`
+        <overlay-trigger type="inline">
+            <sp-button variant="primary" slot="trigger">
+                Open inline overlay
+            </sp-button>
+            <sp-popover slot="click-content" dialog>
+                <overlay-trigger type="modal">
+                    <sp-button variant="primary" slot="trigger">
+                        Open modal overlay
+                    </sp-button>
+                    <sp-popover slot="click-content" dialog>
+                        Modal overlay
+                    </sp-popover>
+                </overlay-trigger>
+            </sp-popover>
+        </overlay-trigger>
+    `;
+};
