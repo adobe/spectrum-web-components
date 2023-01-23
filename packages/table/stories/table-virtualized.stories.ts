fab7424cc647fa65c16f7d2a8f28cdcd065658ea
Najika Yoo <najikayoo@Najikas-MacBook-Pro.local>
2023-01-23 13:27:03 -0800

    fix(table): allow tablebody to be resized via flex-grow

--- packages/table/stories/table-virtualized.stories.ts
+++ packages/table/stories/table-virtualized.stories.ts
@@ -241,7 +241,7 @@ export const virtualizedCustomValue = (args: Properties): TemplateResult => {
         <sp-table
             size="m"
             scroller="true"
-            style="height: 200px"
+            style="height: 300px"
             selects=${args.selects}
             .selected=${args.selected}
             @change=${args.onChange}
