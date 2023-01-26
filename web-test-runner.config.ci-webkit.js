97288d814a2d9031c76706e6f6fc8d4c74bd2284
Westbrook Johnson <westbrook.johnson@gmail.com>
2023-01-26 11:15:26 -0500

    test: refactor WTR config files

--- web-test-runner.config.ci-webkit.js
+++ web-test-runner.config.ci-webkit.js
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import { webkit } from './web-test-runner.config.js';
+import { webkit } from './web-test-runner.utils.js';
 import standard from './web-test-runner.config.ci.js';
 
 standard.browsers = [webkit];
