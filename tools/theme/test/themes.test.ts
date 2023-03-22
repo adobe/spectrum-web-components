5f05c37ab4f0aef5110be470f22bb7edb1329044
Westbrook Johnson <westbrook.johnson@gmail.com>
2023-03-22 09:46:21 -0400

    test(theme): confirm `sp-query-theme` event handling

--- tools/theme/test/themes.test.ts
+++ tools/theme/test/themes.test.ts
@@ -12,7 +12,11 @@ governing permissions and limitations under the License.
 
 import '@spectrum-web-components/theme/sp-theme.js';
 import '@spectrum-web-components/theme/src/themes.js';
-import { Theme, ThemeFragmentMap } from '@spectrum-web-components/theme';
+import {
+    Theme,
+    ThemeData,
+    ThemeFragmentMap,
+} from '@spectrum-web-components/theme';
 import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
 import { css } from '@spectrum-web-components/base';
 
@@ -77,6 +81,45 @@ describe('Themes', () => {
         document.body.append(el);
         expect(testableTheme.instances.has(el), 'third').to.be.true;
         expect(testableTheme.instances.size).to.equal(1);
+    });
+    it('stops propagation on `sp-query-theme` events', async () => {
+        const test = await fixture<Theme>(
+            html`
+                <sp-theme
+                    theme="express"
+                    color="lightest"
+                    scale="medium"
+                    lang="de"
+                >
+                    <sp-theme color="darkest" scale="large" lang="fr">
+                        <div></div>
+                    </sp-theme>
+                </sp-theme>
+            `
+        );
+
+        const el = test.querySelector('div') as HTMLDivElement;
+        const queryThemeDetail: ThemeData = {
+            color: undefined,
+            scale: undefined,
+            lang: undefined,
+            theme: undefined,
+        };
+        const queryThemeEvent = new CustomEvent<ThemeData>('sp-query-theme', {
+            bubbles: true,
+            composed: true,
+            detail: queryThemeDetail,
+            cancelable: true,
+        });
+        el.dispatchEvent(queryThemeEvent);
+        expect(queryThemeDetail.color).to.not.equal('lightest');
+        expect(queryThemeDetail.scale).to.not.equal('medium');
+        expect(queryThemeDetail.lang).to.not.equal('de');
+        expect(queryThemeDetail.theme).to.not.equal('express');
+        expect(queryThemeDetail.color).to.equal('darkest');
+        expect(queryThemeDetail.scale).to.equal('large');
+        expect(queryThemeDetail.lang).to.equal('fr');
+        expect(queryThemeDetail.theme).to.equal('spectrum');
     });
 });
 
