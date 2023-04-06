efc0159d9be10cdfd497a630a2eb4195cd612927
Westbrook Johnson <westbrook.johnson@gmail.com>
2023-04-06 09:08:11 -0400

    fix: support non-flat "color" application

--- packages/swatch/stories/swatch.stories.ts
+++ packages/swatch/stories/swatch.stories.ts
@@ -96,6 +96,10 @@ const template = ({
 };
 
 export const Default = (args: Properties): TemplateResult => template(args);
+export const gradient = (args: Properties): TemplateResult => template(args);
+gradient.args = {
+    color: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+};
 export const mixedValue = (args: Properties): TemplateResult => template(args);
 mixedValue.args = {
     mixedValue: true,
