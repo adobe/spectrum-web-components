5d6f4ea7b3720ba98efebbf8a6abf1308d71def8
Jenn Diaz <jenn.diaz@heysparkbox.com>
2023-04-18 09:45:42 -0600

    refactor(color-loupe): updates position in story

--- packages/color-loupe/stories/color-loupe.stories.ts
+++ packages/color-loupe/stories/color-loupe.stories.ts
@@ -21,6 +21,6 @@ export default {
 
 export const open = (): TemplateResult => {
     return html`
-        <sp-color-loupe open></sp-color-loupe>
+        <sp-color-loupe open style="inset-block-start:25%;"></sp-color-loupe>
     `;
 };
