af911e0fc4ba55f6f4f01825ea619f366828252b
Jian Liao <jian.liao@gmail.com>
2023-05-08 10:49:27 -0700

    feat: initial impl of next.js wrapper for the swc

--- scripts/cem-plugin-react-wrapper.js
+++ scripts/cem-plugin-react-wrapper.js
@@ -48,7 +48,8 @@ function genTsconfigJson() {
 function genPackageJson(
     componentName,
     dependencyPkgName,
-    dependencyPkgVersion
+    dependencyPkgVersion,
+    isIconPkg = false
 ) {
     return `{
         "name": "@swc-react/${componentName}",
@@ -56,10 +57,23 @@ function genPackageJson(
         "publishConfig": {
             "access": "public"
         },
-        "description": "React wrapper of the ${dependencyPkgName} component",
+        "description": "React and Next.js wrapper of the ${dependencyPkgName} component",
         "license": "Apache-2.0",
         "author": "",
-        "main": "index.js",
+        "type": "module",${
+            isIconPkg
+                ? ''
+                : `\n"exports": {
+            ".": {
+                "development": "./index.dev.js",
+                "default": "./index.js"
+            },
+            "./next": {
+                "development": "./next.dev.js",
+                "default": "./next.js"
+            }
+        },`
+        }
         "files": [
             "**/*.d.ts",
             "**/*.js",
@@ -73,6 +87,9 @@ function genPackageJson(
         "dependencies": {
             "@lit-labs/react": "^1.1.1",
             "${dependencyPkgName}": "${dependencyPkgVersion}"
+        },
+        "peerDependencies": {
+            "next": "^13.4.1 || latest",
         }
     }`;
 }
@@ -135,75 +152,56 @@ async function getEvents(decl, declMap, events) {
 }
 
 /**
- * CEM package will invoke this callback function.
- *
- * @param {*} exclude array of excluded component class name
- * @param {*} outDir root output directory for generated code
- * @param {*} prettierConfig prettier library configuration
+ * Generate React wrapper component source code in typescript.
  */
-export default function genReactWrapper({
-    exclude = [],
-    outDir = 'legacy',
-    prettierConfig = {},
-} = {}) {
-    return {
-        name: 'react-wrapper',
-        async packageLinkPhase({ customElementsManifest }) {
-            const { name: pkgName, version: pkgVersion } = JSON.parse(
-                await readFile(`${process.cwd()}/package.json`)
-            );
-            const { modules } = customElementsManifest;
-            const declMap = new Map(
-                modules.flatMap((m) =>
-                    m.declarations.map((decl) => [decl.name, decl])
+async function genReactComponentSourceCode(modules, exclude, pkgName) {
+    const declMap = new Map(
+        modules.flatMap((m) => m.declarations.map((decl) => [decl.name, decl]))
+    );
+
+    const components = modules.flatMap((m) =>
+        m.declarations.filter(
+            (decl) =>
+                !exclude.includes(decl.name) &&
+                (decl.customElement || decl.tagName)
+        )
+    );
+
+    if (components.length === 0) {
+        return;
+    }
+
+    const componentImports = [];
+
+    const fileImports = modules
+        .filter(
+            (m) =>
+                m.exports.length === 1 &&
+                m.exports.some(
+                    (exp) => exp.kind === 'custom-element-definition'
                 )
-            );
-            const components = modules.flatMap((m) =>
-                m.declarations.filter(
-                    (decl) =>
-                        !exclude.includes(decl.name) &&
-                        (decl.customElement || decl.tagName)
-                )
-            );
+        )
+        .map((m) => `import '${pkgName}/${m.path?.replace('.ts', '.js')}';`);
 
-            if (components.length === 0) {
-                return;
-            }
+    const reactComponents = [];
 
-            const componentImports = [];
+    for (let component of components) {
+        componentImports.push(
+            `import { ${component.name} as Sp${component.name} } from '${pkgName}';`
+        );
 
-            const fileImports = modules
-                .filter(
-                    (m) =>
-                        m.exports.length === 1 &&
-                        m.exports.some(
-                            (exp) => exp.kind === 'custom-element-definition'
-                        )
-                )
-                .map(
-                    (m) =>
-                        `import '${pkgName}/${m.path?.replace('.ts', '.js')}';`
-                );
+        const reactComponent = {};
+        reactComponent.name = `${component.name}`;
+        reactComponent.swcComponentName = `Sp${component.name}`;
+        reactComponent.elementName = component.tagName;
+        const events = [];
+        await getEvents(component, declMap, events);
+        reactComponent.events = uniqueBy(events.flat(), 'name');
 
-            const reactComponents = [];
+        reactComponents.push(reactComponent);
+    }
 
-            for (let component of components) {
-                componentImports.push(
-                    `import { ${component.name} as Sp${component.name} } from '${pkgName}';`
-                );
-
-                const reactComponent = {};
-                reactComponent.name = `${component.name}`;
-                reactComponent.swcComponentName = `Sp${component.name}`;
-                reactComponent.elementName = component.tagName;
-                const events = [];
-                await getEvents(component, declMap, events);
-                reactComponent.events = uniqueBy(events.flat(), 'name');
-
-                reactComponents.push(reactComponent);
-            }
-
-            const componentSrc = `/*
+    const componentSrc = `/*
 Copyright 2022 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
@@ -217,11 +215,10 @@ governing permissions and limitations under the License.
 
 import * as React from 'react';
 import { createComponent } from '@lit-labs/react';${
-                reactComponents.flatMap((component) => component.events)
-                    .length > 0
-                    ? "\nimport type { EventName } from '@lit-labs/react';"
-                    : ''
-            }
+        reactComponents.flatMap((component) => component.events).length > 0
+            ? "\nimport type { EventName } from '@lit-labs/react';"
+            : ''
+    }
 ${componentImports.reduce((pre, cur) => pre + cur + '\n', '')}
 ${fileImports.reduce((pre, cur) => pre + cur + '\n', '')}
 
@@ -256,6 +253,88 @@ ${reactComponents.reduce(
     ''
 )}
 `;
+    return componentSrc;
+}
+
+/**
+ * Generate Next.js wrapper component source code in typescript.
+ */
+async function genNextComponentSourceCode(modules, exclude) {
+    const elements = modules.flatMap((m) => {
+        return m.declarations
+            .filter(
+                (d) => d.customElement && d.tagName && !exclude.includes(d.name)
+            )
+            .map((d) => {
+                return {
+                    name: d.name,
+                    tagName: d.tagName,
+                    import: `import { ${d.name} as Sp${d.name} } from '.';`,
+                };
+            });
+    });
+
+    const nextWrapperSource = `/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { ComponentProps } from 'react';
+import dynamic from 'next/dynamic';
+
+${elements.reduce((pre, element) => pre + element.import + '\n', '')}
+
+${elements.reduce(
+    (pre, element) =>
+        pre +
+        `export const ${element.name} = dynamic<JSX.LibraryManagedAttributes<typeof Sp${element.name}, ComponentProps<typeof Sp${element.name}>>>(() => import('.').then(({${element.name}}) => ${element.name}), { ssr: false });` +
+        '\n',
+    ''
+)}`;
+
+    return nextWrapperSource;
+}
+
+/**
+ * CEM package will invoke this callback function.
+ *
+ * @param {*} exclude array of excluded component class name
+ * @param {*} outDir root output directory for generated code
+ * @param {*} prettierConfig prettier library configuration
+ */
+export default function genWrappers({
+    exclude = [],
+    outDir = 'legacy',
+    prettierConfig = {},
+} = {}) {
+    return {
+        name: 'react-wrapper',
+        async packageLinkPhase({ customElementsManifest }) {
+            const { name: pkgName, version: pkgVersion } = JSON.parse(
+                await readFile(`${process.cwd()}/package.json`)
+            );
+            const { modules } = customElementsManifest;
+            const reactComponentSrc = await genReactComponentSourceCode(
+                modules,
+                exclude,
+                pkgName
+            );
+
+            if (!reactComponentSrc) {
+                return;
+            }
+
+            const nextComponentSrc = await genNextComponentSourceCode(
+                modules,
+                exclude
+            );
 
             const componentShortName = pkgName.replace(
                 '@spectrum-web-components/',
@@ -265,7 +344,14 @@ ${reactComponents.reduce(
             const componentPath = resolve(`${outDir}/${componentShortName}`);
             await outputFile(
                 resolve(`${componentPath}/index.ts`),
-                prettier.format(componentSrc, {
+                prettier.format(reactComponentSrc, {
+                    parser: 'typescript',
+                    ...prettierConfig,
+                })
+            );
+            await outputFile(
+                resolve(`${componentPath}/next.ts`),
+                prettier.format(nextComponentSrc, {
                     parser: 'typescript',
                     ...prettierConfig,
                 })
@@ -294,9 +380,9 @@ ${reactComponents.reduce(
 /* Icon wrapper generation ============================================= */
 
 /**
- * Generate Icon component
+ * Generate React wrapper for Icon component
  */
-function genIconComponent(component, id, iconElementName, iconPkg) {
+function genIconReactComponent(component, id, iconElementName, iconPkg) {
     const componentAliasName = `Sp${component}`;
     return `/*
 Copyright 2022 Adobe. All rights reserved.
@@ -323,6 +409,33 @@ export type ${component}Type = ${componentAliasName};
 }
 
 /**
+ * Generate Next.js wrapper for Icon component
+ */
+function genIconNextComponent(displayName) {
+    return `/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import dynamic from 'next/dynamic';
+import { ComponentType } from 'react';
+import type { Icon${displayName}Type } from '../${displayName}';
+    
+export const Icon${displayName}: ComponentType<Partial<Icon${displayName}Type> | { slot: string }> = dynamic<Partial<Icon${displayName}Type> | { slot: string }>(
+    () => import('../${displayName}').then((m) => m.Icon${displayName} as any),
+    { ssr: false}
+);
+`;
+}
+
+/**
  * Core entry function
  */
 export async function generateIconWrapper(iconType) {
@@ -343,17 +456,28 @@ export async function generateIconWrapper(iconType) {
                         `react/${iconType}/${componentName}.ts`
                     ),
                     prettier.format(
-                        genIconComponent(
+                        genIconReactComponent(
                             `Icon${componentName}`,
                             `Icon${id}`,
                             iconElementName,
                             `${iconType}`
                         ),
                         {
-                            parser: 'babel',
+                            parser: 'typescript',
                             ...prettierConfig,
                         }
                     )
+                );
+                await outputFile(
+                    resolve(
+                        __dirname,
+                        '..',
+                        `react/${iconType}/next/${componentName}.ts`
+                    ),
+                    prettier.format(genIconNextComponent(Case.pascal(id)), {
+                        parser: 'typescript',
+                        ...prettierConfig,
+                    })
                 );
             }
 
@@ -363,10 +487,13 @@ export async function generateIconWrapper(iconType) {
 
             await outputFile(
                 resolve(__dirname, '..', `react/${iconType}/package.json`),
-                prettier.format(genPackageJson(iconType, pkgName, pkgVersion), {
-                    parser: 'json',
-                    ...prettierConfig,
-                })
+                prettier.format(
+                    genPackageJson(iconType, pkgName, pkgVersion, true),
+                    {
+                        parser: 'json',
+                        ...prettierConfig,
+                    }
+                )
             );
 
             await outputFile(
