7b4b6457c7986849b7019f3f39ed6a7692a81f72
Venkatesh Josyula <venkateshjo@adobe.com>
2023-07-05 06:51:20 +0530

    feat(opacity-checkerboard): adding new component

---
+++ packages/opacity-checkerboard/src/OpacityCheckerboard.ts
@@ -0,0 +1,45 @@
+/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    CSSResultArray,
+    html,
+    PropertyValues,
+    SpectrumElement,
+    TemplateResult,
+} from '@spectrum-web-components/base';
+
+import styles from './opacity-checkerboard.css.js';
+
+/**
+ * @element sp-opacity-checkerboard
+ */
+export class OpacityCheckerboard extends SpectrumElement {
+    public static override get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    protected override render(): TemplateResult {
+        return html`
+            <div
+                id="checkerboard"
+                style="inline-size: 100px; block-size: 100px;"
+            >
+                <div class="opacityCheckerboard"></div>
+            </div>
+        `;
+    }
+
+    protected override update(changedProperties: PropertyValues<this>): void {
+        super.update(changedProperties);
+    }
+}
