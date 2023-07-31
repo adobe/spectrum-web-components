185e063b5a476286c00e0423a88af4fa52cb995a
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2023-07-31 17:41:49 +0100

    feat(calendar): add calendar component (#3327)
    * feat: add calendar
    
    * feat: use first day of the week according to the locale
    
    * feat: add "disabled" property
    
    * feat: handle "Previous" and "Next" month buttons
    
    * feat: add locale for all stories
    
    * feat: add "min" and "max" date
    
    * refactor: add new render methods
    
    * feat: format day using Intl.NumberFormat
    
    * fix: lit-plugin "no-incompatible-property-type"
    
    * The built in converter doesn't handle the property type Date | undefined
    
    * fix: packages version + property type error
    
    * refactor: add to-do list to README
    
    * feat: add event handlers and slots for icons
    
    * refactor(calendar): update dependencies
    
    * refactor(calendar): changing Lit resource import source
    
    * refactor: add comments and use correct formatters
    
    * refactor: convert property `_locale` to a getter
    
    * refactor: remove `_` from private properties and methods
    
    * refactor: remove unnecessary method
    
    * refactor: remove unnecessary template checking
    
    * refactor: add new private properties
    
    * refactor: code improvements, new comments
    
    * refactor: remove reflect from properties they don't need
    
    * fix: add new package to tsconfig.json
    
    * refactor: use same version of `@internationalized/number` used by other components
    
    * refactor(calendar): improvements and new TODOs

--- tools/base/src/directives.ts
+++ tools/base/src/directives.ts
@@ -13,6 +13,7 @@ governing permissions and limitations under the License.
 export { ifDefined } from 'lit/directives/if-defined.js';
 export { repeat } from 'lit/directives/repeat.js';
 export { classMap } from 'lit/directives/class-map.js';
+export type { ClassInfo } from 'lit/directives/class-map.js';
 export { styleMap } from 'lit/directives/style-map.js';
 export type { StyleInfo } from 'lit/directives/style-map.js';
 export { until } from 'lit/directives/until.js';
