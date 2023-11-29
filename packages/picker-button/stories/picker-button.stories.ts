c9bbdd99ee7c34f21adabd7869e4a2bac8dbba0c
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2023-11-29 15:51:25 +0000

    fix: picker button import

--- packages/picker-button/stories/picker-button.stories.ts
+++ packages/picker-button/stories/picker-button.stories.ts
@@ -13,9 +13,9 @@ governing permissions and limitations under the License.
 import { html, TemplateResult } from '@spectrum-web-components/base';
 
 import { argTypes, StoryArgs, Template } from './index.js';
-import '../sp-picker-button.js';
 
 import '@spectrum-web-components/icons-workflow/icons/sp-icon-add.js';
+import '@spectrum-web-components/picker-button/sp-picker-button.js';
 
 export default {
     title: 'Picker Button',
