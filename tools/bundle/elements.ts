279a78e20880f7f60b018862e264a41e57d86136
Paulo de Tarso Furtado Machado <paulodetarsofm@yahoo.com.br>
2023-11-29 17:49:04 +0000

    feat: add `date-time-picker` component

--- tools/bundle/elements.ts
+++ tools/bundle/elements.ts
@@ -32,6 +32,7 @@ import '@spectrum-web-components/color-handle/sp-color-handle.js';
 import '@spectrum-web-components/color-loupe/sp-color-loupe.js';
 import '@spectrum-web-components/color-slider/sp-color-slider.js';
 import '@spectrum-web-components/color-wheel/sp-color-wheel.js';
+import '@spectrum-web-components/date-time-picker/sp-date-time-picker.js';
 import '@spectrum-web-components/dialog/sp-dialog.js';
 import '@spectrum-web-components/dialog/sp-dialog-base.js';
 import '@spectrum-web-components/dialog/sp-dialog-wrapper.js';
