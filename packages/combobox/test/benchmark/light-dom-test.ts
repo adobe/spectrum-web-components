56ce9237dd4b62e23b9721c9d0acd0d6ae145537
Najika Halsema Yoo <44980010+najikahalsema@users.noreply.github.com>
2023-12-21 12:55:52 -0800

    chore: add benchmark test for lightdom combobox (#3898)
    * chore: add benchmark test for lightdom combobox
    
    * chore: add object version of benchmark test
    
    * chore: rename files
    
    ---------
    
    Co-authored-by: Najika Yoo <naj.halsema@gmail.com>

--- packages/combobox/test/benchmark/light-dom-test.ts
+++ packages/combobox/test/benchmark/light-dom-test.ts
@@ -14,11 +14,16 @@ import '@spectrum-web-components/combobox/sp-combobox.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
 import { html } from '@spectrum-web-components/base';
 import { measureFixtureCreation } from '../../../../test/benchmark/helpers.js';
-import { comboboxOptions } from '../index.js';
+import { countryList } from '../index.js';
 
 measureFixtureCreation(html`
-    <sp-combobox
-        .autocomplete=${'none'}
-        .options=${comboboxOptions}
-    ></sp-combobox>
+    <sp-combobox .autocomplete=${'none'}>
+        ${countryList.map(
+            (option, index) => html`
+                <sp-menu-item id=${index} value=${option}>
+                    ${option}
+                </sp-menu-item>
+            `
+        )}
+    </sp-combobox>
 `);
