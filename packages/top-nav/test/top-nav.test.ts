378a23052f8fe505312d4da91e5418fc32655d8c
Westbrook Johnson <westbrook.johnson@gmail.com>
2024-02-05 08:16:16 -0500

    chore(top-nav): clean up CSS implementation for user interactions

--- packages/top-nav/test/top-nav.test.ts
+++ packages/top-nav/test/top-nav.test.ts
@@ -16,6 +16,7 @@ import { TopNav, TopNavItem } from '@spectrum-web-components/top-nav';
 import { Default, Selected } from '../stories/top-nav.stories.js';
 import { spy } from 'sinon';
 import { testForLitDevWarnings } from '../../../test/testing-helpers';
+import { sendMouse } from '../../../test/plugins/browser.js';
 
 describe('TopNav', () => {
     testForLitDevWarnings(async () => await fixture<TopNav>(Default()));
@@ -100,6 +101,34 @@ describe('TopNavItem', () => {
         await elementUpdated(el);
 
         el.click();
+
+        expect(clickSpy.called).to.be.true;
+        expect(clickSpy.calledWith(anchor)).to.be.true;
+    });
+    it('`<a>` accepts click across full item area', async () => {
+        const clickSpy = spy();
+        const test = await fixture<TopNav>(Selected());
+        const el = test.querySelector(
+            'sp-top-nav-item:nth-of-type(4)'
+        ) as TopNavItem;
+        const anchor = el.focusElement;
+        test.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+            const target = event.composedPath()[0];
+            clickSpy(target);
+        });
+        await elementUpdated(el);
+        const rect = el.getBoundingClientRect();
+
+        await sendMouse({
+            steps: [
+                {
+                    type: 'click',
+                    position: [rect.left + rect.width / 2, rect.top + 1],
+                },
+            ],
+        });
+        await elementUpdated(test);
 
         expect(clickSpy.called).to.be.true;
         expect(clickSpy.calledWith(anchor)).to.be.true;
