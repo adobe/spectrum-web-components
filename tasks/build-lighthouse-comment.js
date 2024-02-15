bfa9b4a023c8dab7a66a6258c7a7560a7e1aee94
Westbrook Johnson <wesjohns@adobe.com>
2024-02-15 13:41:59 -0500

    ci: expand on lighthouse usage (#4046)

--- tasks/build-lighthouse-comment.js
+++ tasks/build-lighthouse-comment.js
@@ -11,10 +11,130 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+import fs from 'fs';
+import prettyBytes from 'pretty-bytes';
+
 const latestURL = 'https://opensource.adobe.com/spectrum-web-components/';
 const mainURL = 'https://main--spectrum-web-components.netlify.app/';
+const winnerSign = ' 🏆';
 
-export const buildLighthouseComment = (links, manifest) => {
+const totalIndex = 0;
+const scriptIndex = 2;
+const stylesheetIndex = 3;
+const documentIndex = 4;
+const thirdPartyIndex = 8;
+
+const formatResourceByIndex = (index, resource, resourcesRaw, winner) =>
+    resource === 'transferSize'
+        ? prettyBytes(resourcesRaw[index][resource], {
+              minimumFractionDigits: 2,
+          }) + (winner ? winnerSign : '')
+        : resourcesRaw[index][resource] + (winner ? winnerSign : '');
+
+const formatResources = (resource, resourcesRaw, winners) => ({
+    total: formatResourceByIndex(
+        totalIndex,
+        resource,
+        resourcesRaw,
+        winners.total
+    ),
+    scripts: formatResourceByIndex(
+        scriptIndex,
+        resource,
+        resourcesRaw,
+        winners.scripts
+    ),
+    stylesheet: formatResourceByIndex(
+        stylesheetIndex,
+        resource,
+        resourcesRaw,
+        winners.stylesheet
+    ),
+    document: formatResourceByIndex(
+        documentIndex,
+        resource,
+        resourcesRaw,
+        winners.document
+    ),
+    thirdParty: formatResourceByIndex(
+        thirdPartyIndex,
+        resource,
+        resourcesRaw,
+        winners.thirdParty
+    ),
+});
+
+const formatAllResources = (resourcesRaw, winners) => ({
+    size: formatResources('transferSize', resourcesRaw, winners.size),
+    count: formatResources('requestCount', resourcesRaw, winners.count),
+});
+
+const getWinnerByIndex = (index, test, context, competitorA, competitorB) => {
+    return (
+        context[index][test] < competitorA[index][test] &&
+        context[index][test] < competitorB[index][test]
+    );
+};
+
+const getWinners = (test, context, competitorA, competitorB) => ({
+    total: getWinnerByIndex(
+        totalIndex,
+        test,
+        context,
+        competitorA,
+        competitorB
+    ),
+    scripts: getWinnerByIndex(
+        scriptIndex,
+        test,
+        context,
+        competitorA,
+        competitorB
+    ),
+    stylesheet: getWinnerByIndex(
+        stylesheetIndex,
+        test,
+        context,
+        competitorA,
+        competitorB
+    ),
+    document: getWinnerByIndex(
+        documentIndex,
+        test,
+        context,
+        competitorA,
+        competitorB
+    ),
+    thirdParty: getWinnerByIndex(
+        thirdPartyIndex,
+        test,
+        context,
+        competitorA,
+        competitorB
+    ),
+});
+
+const getAllWiners = (context, competitorA, competitorB) => ({
+    size: getWinners('transferSize', context, competitorA, competitorB),
+    count: getWinners('requestCount', context, competitorA, competitorB),
+});
+
+const compareResourceUsage = (branch, main, latest) => {
+    const branchWinners = getAllWiners(branch, latest, main);
+    const mainWinners = getAllWiners(main, branch, latest);
+    const latestWinners = getAllWiners(latest, main, branch);
+    const branchResources = formatAllResources(branch, branchWinners);
+    const mainResources = formatAllResources(main, mainWinners);
+    const latestResources = formatAllResources(latest, latestWinners);
+
+    return {
+        branchResources,
+        mainResources,
+        latestResources,
+    };
+};
+
+export const buildLighthouseComment = (links, manifest, assertionResults) => {
     const report = {};
     Object.keys(links).forEach((key) => {
         if (key === mainURL) {
@@ -31,13 +151,28 @@ export const buildLighthouseComment = (links, manifest) => {
             result.url !== mainURL &&
             result.url !== latestURL
     );
-    const latest = manifest.find(
-        (result) => result.isRepresentativeRun && result.url === latestURL
-    );
     const main = manifest.find(
         (result) => result.isRepresentativeRun && result.url === mainURL
     );
+    const latest = manifest.find(
+        (result) => result.isRepresentativeRun && result.url === latestURL
+    );
+    const branchResourcesRaw = JSON.parse(
+        fs.readFileSync(branch.jsonPath, 'utf8')
+    ).audits['resource-summary'].details.items;
+    const mainResourcesRaw = JSON.parse(fs.readFileSync(main.jsonPath, 'utf8'))
+        .audits['resource-summary'].details.items;
+    const latestResourcesRaw = JSON.parse(
+        fs.readFileSync(latest.jsonPath, 'utf8')
+    ).audits['resource-summary'].details.items;
+    const { branchResources, mainResources, latestResources } =
+        compareResourceUsage(
+            branchResourcesRaw,
+            mainResourcesRaw,
+            latestResourcesRaw
+        );
 
+    /* eslint-disable prettier/prettier */
     const comment = `## Lighthouse scores
 
 | Category | Latest ([report](${report.latest})) | Main ([report](${report.main})) | Branch ([report](${report.branch})) |
@@ -48,8 +183,33 @@ export const buildLighthouseComment = (links, manifest) => {
 | SEO | ${latest.summary.seo} | ${main.summary.seo} | ${branch.summary.seo} |
 | PWA | ${latest.summary.pwa} | ${main.summary.pwa} | ${branch.summary.pwa} |
 
-[Lighthouse](https://github.com/GoogleChrome/lighthouse) scores comparing the documentation site built from the PR ("Branch") to that of the production documentation site ("Latest") and the build currently on <code>main</code> ("Main"). Higher scores are better, but *note that the SEO scores on Netlify URLs are artifically constrained to 0.92.*
-`;
+<details>
+    <summary>What is this?</summary>
 
+[Lighthouse](https://github.com/GoogleChrome/lighthouse) scores comparing the documentation site built from the PR ("Branch") to that of the production documentation site ("Latest") and the build currently on <code>main</code> ("Main"). Higher scores are better, but *note that the SEO scores on Netlify URLs are artifically constrained to 0.92.*
+
+</details>
+
+### Transfer Size
+
+| Category | Latest | Main | Branch |
+|---|---|---|---|
+| Total | ${latestResources.size.total} | ${mainResources.size.total} | ${branchResources.size.total} |
+| Scripts | ${latestResources.size.scripts} | ${mainResources.size.scripts} | ${branchResources.size.scripts} |
+| Stylesheet | ${latestResources.size.stylesheet} | ${mainResources.size.stylesheet} | ${branchResources.size.stylesheet} |
+| Document | ${latestResources.size.document} | ${mainResources.size.document} | ${branchResources.size.document} |
+| Third Party | ${latestResources.size.thirdParty} | ${mainResources.size.thirdParty} | ${branchResources.size.thirdParty} |
+
+### Request Count
+
+| Category | Latest | Main | Branch |
+|---|---|---|---|
+| Total | ${latestResources.count.total} | ${mainResources.count.total} | ${branchResources.count.total} |
+| Scripts | ${latestResources.count.scripts} | ${mainResources.count.scripts} | ${branchResources.count.scripts} |
+| Stylesheet | ${latestResources.count.stylesheet} | ${mainResources.count.stylesheet} | ${branchResources.count.stylesheet} |
+| Document | ${latestResources.count.document} | ${mainResources.count.document} | ${branchResources.count.document} |
+| Third Party | ${latestResources.count.thirdParty} | ${mainResources.count.thirdParty} | ${branchResources.count.thirdParty} |
+`;
+    /* eslint-enable prettier/prettier */
     return comment;
 };
