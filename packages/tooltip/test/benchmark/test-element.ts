32e1b4be5bf0bff960fe1f29dcf682a3f54cac4f
Westbrook Johnson <westbrook.johnson@gmail.com>
2024-03-18 15:00:56 -0400

    test(overlay, tooltip): clarify comparison test contexts

--- packages/tooltip/test/benchmark/test-element.ts
+++ packages/tooltip/test/benchmark/test-element.ts
@@ -12,12 +12,13 @@ governing permissions and limitations under the License.
 
 import '@spectrum-web-components/tooltip/sp-tooltip.js';
 import '@spectrum-web-components/action-button/sp-action-button.js';
+import '@spectrum-web-components/overlay/sp-overlay.js';
 import { html } from 'lit';
 import { measureFixtureCreation } from '../../../../test/benchmark/helpers.js';
 
 measureFixtureCreation(html`
-    <sp-action-button>
-        I'm a button...
-        <sp-tooltip self-managed>Tip me!</sp-tooltip>
-    </sp-action-button>
+    <sp-action-button id="button">I'm a button...</sp-action-button>
+    <sp-overlay trigger="button@hover" type="hint">
+        <sp-tooltip>Tip me!</sp-tooltip>
+    </sp-overlay>
 `);
