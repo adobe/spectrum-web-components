7401d7aba308d4421ae1b77bd696e06d03697d9a
Loredana Spataru <lspataru@Loredanas-MacBook-Pro-2.local>
2024-04-15 14:25:28 +0300

    feat(alert-banner): add missing dependencies

--- packages/alert-banner/stories/alert-banner.stories.ts
+++ packages/alert-banner/stories/alert-banner.stories.ts
@@ -10,9 +10,10 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 import { html, TemplateResult } from '@spectrum-web-components/base';
+import { AlertBannerVariants } from '@spectrum-web-components/alert-banner';
+
 import '@spectrum-web-components/alert-banner/sp-alert-banner.js';
 import '@spectrum-web-components/button/sp-button.js';
-import { AlertBannerVariants } from '../src/AlertBanner';
 
 interface Properties {
     text: string;
