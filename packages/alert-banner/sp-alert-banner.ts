ca1fd66e4dde8508859244a53778f7bacd7bbc01
Loredana Spataru <lspataru@Loredanas-MacBook-Pro-2.local>
2024-04-15 13:34:25 +0300

    feat(alert-banner): use define element to register component

--- packages/alert-banner/sp-alert-banner.ts
+++ packages/alert-banner/sp-alert-banner.ts
@@ -10,8 +10,9 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 import { AlertBanner } from './src/AlertBanner.js';
+import { defineElement } from '@spectrum-web-components/base/src/define-element.js';
 
-customElements.define('sp-alert-banner', AlertBanner);
+defineElement('sp-alert-banner', AlertBanner);
 
 declare global {
     interface HTMLElementTagNameMap {
