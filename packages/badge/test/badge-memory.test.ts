ac67fa2ad6d8d642b4894b7fa47a3888d5593a2f
TaraT <ttomar@adobe.com>
2024-04-30 18:07:39 +0530

    test: added memory tests for missing packages (#4264)
    * chore: added memory tests for missing packages
    
    * chore: completed memory testing for alll packages
    
    * chore: added mem test for color elements
    
    * chore: color area test updated
    
    * chore: simlified mem tests construct
    
    * chore: reduce concurrency in mem tooling in ci test
    
    * chore: reduce concurrency in mem tooling in ci test in chromium
    
    ---------
    
    Co-authored-by: Rajdeep Chandra <rajdeepchandra@Rajdeeps-MacBook-Pro-2.local>
    Co-authored-by: Rajdeep Chandra <rajdeepchandra@rajdeeps-mbp-2.corp.adobe.com>

--- packages/badge/test/badge-memory.test.ts
+++ packages/badge/test/badge-memory.test.ts
@@ -10,9 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { fixture } from '@open-wc/testing';
 import { Default } from '../stories/badge.stories.js';
-import { Badge } from '@spectrum-web-components/badge';
 import { testForMemoryLeaks } from '../../../test/testing-helpers.js';
 
-testForMemoryLeaks(async () => await fixture<Badge>(Default()));
+testForMemoryLeaks(Default());
