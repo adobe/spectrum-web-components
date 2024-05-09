271a436f298ce49c98f9cfdfb7cab21eaca56bac
Rajdeep Chandra <rajdeepchandra@Rajdeeps-MacBook-Pro-2.local>
2024-05-09 17:28:01 +0530

    chore: reverted swatch property definition

--- packages/swatch/src/Swatch.ts
+++ packages/swatch/src/Swatch.ts
@@ -73,7 +73,7 @@ export class Swatch extends SizedMixin(Focusable, {
     @property({ reflect: true })
     public border: SwatchBorder;
 
-    @property({ type: String, reflect: true })
+    @property()
     public color = '';
 
     @property()
