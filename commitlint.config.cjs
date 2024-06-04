f8bc9b503d934caa924139a1c6c8a1ac7ff1c68e
blunteshwar <piyush17303@iiitd.ac.in>
2024-06-04 19:42:36 +0530

    feat: migrated to changesets from lerna

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -10,8 +10,5 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 module.exports = {
-    extends: [
-        '@commitlint/config-conventional',
-        '@commitlint/config-lerna-scopes',
-    ],
+    extends: ['@commitlint/config-conventional'],
 };
