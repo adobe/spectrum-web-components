e6953faf4f505f941730b592c78609fcd34a12b3
castastrophe <castastrophe@users.noreply.github.com>
2024-07-22 11:31:19 -0400

    feat: add an optional chromatic vrt action

--- packages/dialog/stories/dialog-wrapper.stories.ts
+++ packages/dialog/stories/dialog-wrapper.stories.ts
@@ -673,3 +673,8 @@ lazyHero.args = {
 lazyHero.swc_vrt = {
     skip: true,
 };
+
+lazyHero.parameters = {
+    // Disables Chromatic's snapshotting on a global level
+    chromatic: { disableSnapshot: true },
+};
