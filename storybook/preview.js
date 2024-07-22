7de5f497539e3006c01553feae6b4118bb938b1f
castastrophe <castastrophe@users.noreply.github.com>
2024-07-22 12:16:54 -0400

    feat: use globals to enable VRT modes in chromatic

--- storybook/preview.js
+++ storybook/preview.js
@@ -15,6 +15,82 @@ import cem from './custom-elements.json';
 
 setCustomElementsManifest(cem);
 
+export const globalTypes = {
+    system: {
+        title: 'Design context',
+        description: 'The variation of Spectrum to use in the component',
+        defaultValue: 'spectrum',
+        type: 'string',
+        showName: true,
+        toolbar: {
+            items: [
+                { value: 'spectrum', title: 'Spectrum 2', right: 'default' },
+                { value: 'legacy', title: 'Spectrum 1', right: 'legacy' },
+                { value: 'express', title: 'Express' },
+            ],
+            dynamicTitle: true,
+        },
+    },
+    color: {
+        title: 'Color',
+        description: 'Controls the color context of the component',
+        defaultValue: 'light',
+        icon: 'paintbrush',
+        type: 'string',
+        toolbar: {
+            items: [
+                { value: 'light', title: 'Light', right: 'default' },
+                { value: 'dark', title: 'Dark' },
+            ],
+            dynamicTitle: true,
+        },
+    },
+    scale: {
+        title: 'Platform scale',
+        description: 'Controls the platform scale of the component',
+        defaultValue: 'medium',
+        type: 'string',
+        toolbar: {
+            items: [
+                {
+                    value: 'medium',
+                    title: 'Medium',
+                    right: 'default',
+                    icon: 'browser',
+                },
+                { value: 'large', title: 'Large', icon: 'mobile' },
+            ],
+            dynamicTitle: true,
+        },
+    },
+    textDirection: {
+        title: 'Text direction',
+        description: 'Direction of the content flow',
+        defaultValue: 'ltr',
+        type: 'string',
+        toolbar: {
+            items: [
+                { value: 'ltr', title: 'Left to right' },
+                { value: 'rtl', title: 'Right to left' },
+            ],
+            dynamicTitle: true,
+        },
+    },
+    reducedMotion: {
+        title: 'Reduce motion',
+        description: 'Reduce animation and transitions',
+        defaultValue: false,
+        type: 'boolean',
+        toolbar: {
+            items: [
+                { value: false, title: 'Default', icon: 'play' },
+                { value: true, title: 'Reduced motion', icon: 'stop' },
+            ],
+            dynamicTitle: true,
+        },
+    },
+};
+
 export const parameters = {
     docs: { hidden: true },
     controls: { expanded: true },
@@ -33,6 +109,21 @@ export const parameters = {
         forcedColors: 'none',
         prefersReducedMotion: 'no-preference',
         pauseAnimationAtEnd: true,
+        modes: {
+            'Context: Spectrum 1': {
+                scale: 'medium',
+                color: 'light',
+                textDirection: 'ltr',
+                context: 'spectrum1',
+            },
+            'Context: Express': {
+                context: 'express',
+            },
+            'Dark | RTL': {
+                color: 'dark',
+                textDirection: 'rtl',
+            },
+        },
     },
 };
 
