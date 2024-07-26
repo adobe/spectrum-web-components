bdcdabd011f105502e579ba10f070db6c642035c
Nikki Massaro <massaro@adobe.com>
2024-07-26 14:57:33 -0400

    chore: removed console log

--- scripts/generate-tokens-wrapper.js
+++ scripts/generate-tokens-wrapper.js
@@ -186,7 +186,7 @@ const processPackages = async (tokensDir, index) => {
                 type,
                 packagename + '.css'
             );
-            console.log(cssFilePath);
+
             // check if cssFilePath exists
             if (fs.existsSync(cssFilePath)) {
                 let content = fs.readFileSync(cssFilePath, 'utf8');
