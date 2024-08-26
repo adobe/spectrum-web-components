278d93723a18c5cea310c0f3e65d75d1dea81499
Ruben Carvalho <rcarvalho@adobe.com>
2024-08-26 12:30:14 +0200

    chore: fix choachmark

--- packages/coachmark/stories/coachmark.stories.ts
+++ packages/coachmark/stories/coachmark.stories.ts
@@ -309,7 +309,7 @@ export const withShortCut = (props: Properties): TemplateResult => {
             secondary-cta="Previous"
             current-step=${currentStep}
             total-steps=${totalSteps}
-            shortcut-key="M"
+            shortcut-key="Z"
             .content=${{
                 title: 'Coachmark Shortcut',
                 description:
