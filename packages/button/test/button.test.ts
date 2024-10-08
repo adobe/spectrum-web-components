4d90dfd7a24004ddbea4f06b88b98755af10c220
Ruben Carvalho <rcarvalho@adobe.com>
2024-10-08 12:46:38 +0200

    chore: add button deprecation dev warning test

--- packages/button/test/button.test.ts
+++ packages/button/test/button.test.ts
@@ -75,7 +75,29 @@ describe('Button', () => {
                 },
             });
         });
+        it('warns in devMode when white/black static is provided', async () => {
+            const el = await fixture<Button>(html`
+                <sp-button tabindex="0" static="white">Button</sp-button>
+            `);
+
+            await elementUpdated(el);
+            expect(consoleWarnStub.called).to.be.true;
+
+            const spyCall = consoleWarnStub.getCall(0);
+            expect(
+                (spyCall.args.at(0) as string).includes('deprecated'),
+                'confirm deprecated static warning'
+            ).to.be.true;
+            expect(spyCall.args.at(-1), 'confirm `data` shape').to.deep.equal({
+                data: {
+                    localName: 'sp-button',
+                    type: 'api',
+                    level: 'deprecation',
+                },
+            });
+        });
     });
+
     it('loads default', async () => {
         const el = await fixture<Button>(html`
             <sp-button tabindex="0">Button</sp-button>
