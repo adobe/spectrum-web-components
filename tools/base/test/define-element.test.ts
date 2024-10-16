d428f8d5e88a44b5711db42af57a8c27416f0935
Rúben Carvalho <rubcar@sapo.pt>
2024-10-16 17:21:35 +0200

    chore: remove banner component (#4723)

--- tools/base/test/define-element.test.ts
+++ tools/base/test/define-element.test.ts
@@ -40,7 +40,6 @@ const elements = {
     'sp-asset': () => import('@spectrum-web-components/asset/sp-asset.js'),
     'sp-avatar': () => import('@spectrum-web-components/avatar/sp-avatar.js'),
     'sp-badge': () => import('@spectrum-web-components/badge/sp-badge.js'),
-    'sp-banner': () => import('@spectrum-web-components/banner/sp-banner.js'),
     'sp-dialog-wrapper': () =>
         import('@spectrum-web-components/dialog/sp-dialog-wrapper.js'),
     'sp-dialog': () => import('@spectrum-web-components/dialog/sp-dialog.js'),
