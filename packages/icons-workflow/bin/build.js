5099a83e1e811589b0e416392eb73f3fcda6b255
TarunAdobe <ttomar@adobe.com>
2024-11-07 22:32:38 +0530

    chore: update golden image hash

--- packages/icons-workflow/bin/build.js
+++ packages/icons-workflow/bin/build.js
@@ -85,6 +85,14 @@ let manifestListings = `\r\nexport const iconManifest = [\r\n`;
 
 const defaultIconImport = `import { DefaultIcon as AlternateIcon } from '../DefaultIcon.js';\r\n`;
 
+// Temporary replacements for a few icon names that have different names in the new S2 icon set
+const replacements = {
+    UnLink: 'Unlink',
+    TextStrikeThrough: 'TextStrikethrough',
+    ChevronDownSize300: 'ChevronDown',
+    CheckmarkSize300: 'Checkmark',
+};
+
 // A function that process the raw svg name and returns the component name
 export const getComponentName = (i) => {
     let id = path
@@ -94,13 +102,6 @@ export const getComponentName = (i) => {
     if (i.startsWith('Ad')) {
         id = i.replace(/^Ad/, '') + 'Advert';
     }
-    const replacements = {
-        UnLink: 'Unlink',
-        TextStrikeThrough: 'TextStrikethrough',
-        github: 'GitHub',
-        ChevronDownSize300: 'ChevronDown',
-        CheckmarkSize300: 'Checkmark',
-    };
     return Case.pascal(replacements[id] || id);
 };
 
@@ -117,16 +118,10 @@ async function buildIcons(icons, tag, iconsNameList) {
             id += 'Advert';
         }
 
-        const replacements = {
-            UnLink: 'Unlink',
-            TextStrikeThrough: 'TextStrikethrough',
-            ChevronDownSize300: 'ChevronDown',
-            CheckmarkSize300: 'Checkmark',
-        };
+        // use replacements for icons that have different names in the icon set and update the id
+        id = replacements[id] || id;
 
-        id = Case.pascal(replacements[id] || id);
-
-        let ComponentName = id === 'github' ? 'GitHub' : Case.pascal(id);
+        const ComponentName = Case.pascal(id);
 
         const $ = load(svg, {
             xmlMode: true,
