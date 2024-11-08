8322a6c244582a9074701471bc78fe24ed6b60cb
Ruben Carvalho <rcarvalho@adobe.com>
2024-11-08 17:17:07 +0100

    chore: revert vrt trigger change

--- packages/coachmark/stories/coachmark.stories.ts
+++ packages/coachmark/stories/coachmark.stories.ts
@@ -309,7 +309,7 @@ export const withShortCut = (props: Properties): TemplateResult => {
             secondary-cta="Previous"
             current-step=${currentStep}
             total-steps=${totalSteps}
-            shortcut-key="X"
+            shortcut-key="Z"
             .content=${{
                 title: 'Coachmark Shortcut',
                 description:
