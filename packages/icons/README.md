cf70eda64fb616c9bb2df2726b3548439c7f83dd
TaraT <ttomar@adobe.com>
2025-01-14 17:06:34 +0530

    chore(icons): add deprecation notice and warning (#5009)
    * chore(icons): add deprecation notice and warning
    
    * chore(icons): add test for deprecated usage
    
    * chore(icons): update deprecated notice doc
    
    Co-authored-by: Rúben Carvalho <rubcar@sapo.pt>
    
    ---------
    
    Co-authored-by: Rúben Carvalho <rubcar@sapo.pt>

--- packages/icons/README.md
+++ packages/icons/README.md
@@ -24,6 +24,14 @@ When looking to leverage the `IconsMedium` or `IconsLarge` base classes as a typ
 import { IconsMedium, IconsLarge } from '@spectrum-web-components/icons';
 ```
 
+### Deprecated
+
+The `Icons` package has been deprecated as part of the removal of the `Iconset` package from the library and will be removed in an upcoming release. To optimize your build and ensure smaller bundles and higher performance for your users, consider using techniques that include only the icons actually used in your application. For Spectrum icons, you can use [UI Icons](../icons-ui/) or [Workflow Icons](../icons-workflow/).
+
+For non-Spectrum icons, you can still:
+1. Slot SVG or image content into an [`sp-icon` element](../icon/), or
+2. Sanitize the SVG and convert it to a template literal to use within the `render()` method of an extension of `IconBase` to create your own custom named icon element.```
+
 ### Accessibility
 
 Review the accessibility guidelines for the [icon](../icon#accessibility-guidelines).
--- packages/icons/test/icons.test.ts
+++ packages/icons/test/icons.test.ts
@@ -13,14 +13,13 @@ import '@spectrum-web-components/icons/sp-icons-medium.js';
 import { IconsLarge, IconsMedium } from '../';
 import IconsetSVG from '../src/icons-large.svg.js';
 import { elementUpdated, expect, fixture, html } from '@open-wc/testing';
+import { stub } from 'sinon';
 
 describe('icons', () => {
     it('large', async () => {
-        const el = await fixture<IconsLarge>(
-            html`
-                <sp-icons-large></sp-icons-large>
-            `
-        );
+        const el = await fixture<IconsLarge>(html`
+            <sp-icons-large></sp-icons-large>
+        `);
 
         await elementUpdated(el);
 
@@ -28,11 +27,9 @@ describe('icons', () => {
         expect(el.getIconList().length).to.be.above(0);
     });
     it('medium', async () => {
-        const el = await fixture<IconsMedium>(
-            html`
-                <sp-icons-medium></sp-icons-medium>
-            `
-        );
+        const el = await fixture<IconsMedium>(html`
+            <sp-icons-medium></sp-icons-medium>
+        `);
 
         await elementUpdated(el);
 
@@ -40,15 +37,71 @@ describe('icons', () => {
         expect(el.getIconList().length).to.be.above(0);
     });
     it('listens to slotchange events', async () => {
-        const el = await fixture<IconsMedium>(
-            html`
-                <sp-icons-medium>${IconsetSVG}</sp-icons-medium>
-            `
-        );
+        const el = await fixture<IconsMedium>(html`
+            <sp-icons-medium>${IconsetSVG}</sp-icons-medium>
+        `);
 
         await elementUpdated(el);
 
         expect(el).to.not.equal(undefined);
         expect(el.getIconList().length).to.equal(48);
+    });
+});
+
+describe('dev mode', () => {
+    let consoleWarnStub!: ReturnType<typeof stub>;
+    before(() => {
+        window.__swc.verbose = true;
+        consoleWarnStub = stub(console, 'warn');
+    });
+    afterEach(() => {
+        consoleWarnStub.resetHistory();
+    });
+    after(() => {
+        window.__swc.verbose = false;
+        consoleWarnStub.restore();
+    });
+
+    it('warns in devMode for deprecated usage - medium', async () => {
+        const el = await fixture<IconsMedium>(html`
+            <sp-icons-medium>${IconsetSVG}</sp-icons-medium>
+        `);
+
+        await elementUpdated(el);
+        expect(consoleWarnStub.called).to.be.true;
+
+        const spyCall = consoleWarnStub.getCall(0);
+        expect(
+            (spyCall.args.at(0) as string).includes('deprecated'),
+            'confirm deprecated variant warning'
+        ).to.be.true;
+        expect(spyCall.args.at(-1), 'confirm `data` shape').to.deep.equal({
+            data: {
+                localName: 'sp-icons-medium',
+                type: 'api',
+                level: 'deprecation',
+            },
+        });
+    });
+    it('warns in devMode for deprecated usage - large', async () => {
+        const el = await fixture<IconsLarge>(html`
+            <sp-icons-large>${IconsetSVG}</sp-icons-large>
+        `);
+
+        await elementUpdated(el);
+        expect(consoleWarnStub.called).to.be.true;
+
+        const spyCall = consoleWarnStub.getCall(0);
+        expect(
+            (spyCall.args.at(0) as string).includes('deprecated'),
+            'confirm deprecated variant warning'
+        ).to.be.true;
+        expect(spyCall.args.at(-1), 'confirm `data` shape').to.deep.equal({
+            data: {
+                localName: 'sp-icons-large',
+                type: 'api',
+                level: 'deprecation',
+            },
+        });
     });
 });
