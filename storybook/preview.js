b1b4350418b0f3f021cea6735becc36523451452
blunteshwar <piyush17303@iiitd.ac.in>
2025-01-15 21:17:10 +0530

    chore: brought upto main

--- storybook/preview.js
+++ storybook/preview.js
@@ -11,6 +11,7 @@ governing permissions and limitations under the License.
 */
 import { setCustomElementsManifest } from '@storybook/web-components';
 import { swcThemeDecorator } from '@spectrum-web-components/story-decorator/decorator.js';
+
 import cem from './custom-elements.json';
 
 setCustomElementsManifest(cem);
