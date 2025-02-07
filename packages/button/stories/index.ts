2cc66f879353a77a80673bd775895e016a5f840e
TarunAdobe <ttomar@adobe.com>
2025-02-07 14:27:56 +0530

    chore: update menu popover to support action menu

--- packages/button/stories/index.ts
+++ packages/button/stories/index.ts
@@ -98,8 +98,11 @@ export const makeOverBackground =
         return html`
             <div
                 style="
-                    --mod-actionbutton-static-content-color: ${color};
-                    --mod-button-static-content-color: ${color};
+                    --mod-actionbutton-content-color-default: ${color};
+                    --mod-button-content-color-default: ${color};
+                    --mod-button-border-color-default: ${color};
+                    --mod-button-content-color-disabled: ${color};
+                    --mod-button-border-color-disabled: ${color};
                     background-color: ${color};
                     padding: calc(var(--swc-scale-factor) * 14px) calc(var(--swc-scale-factor) * 20px);
                     display: inline-block;
