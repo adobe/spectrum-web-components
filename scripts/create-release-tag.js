e43581357654d9705624554f6ad21aa93fd9275b
blunteshwar <piyush17303@iiitd.ac.in>
2025-02-10 17:27:28 +0530

    chore: minor fix

--- scripts/create-release-tag.js
+++ scripts/create-release-tag.js
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-process.emitWarning = () => {}; // Suppresses the warning
+
 import { execSync } from 'child_process';
 import pkg from '../packages/button/package.json' assert { type: 'json' };
 
