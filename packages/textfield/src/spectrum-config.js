6e5b75c5b91c1565cd34a185bd48b13ae9d9921a
Rajdeep Chandra <rajdeepchandra@Rajdeeps-MacBook-Pro-2.local>
2025-02-12 15:25:30 +0530

    chore: updated textfield sizes from config

--- packages/textfield/src/spectrum-config.js
+++ packages/textfield/src/spectrum-config.js
@@ -45,6 +45,48 @@ export default {
                 },
                 // Default to `size='m'` without needing the attribute
                 converter.classToHost('spectrum-Textfield--sizeM'),
+                {
+                    find: [
+                        builder.class('spectrum-Textfield--sizeS'),
+                    ],
+                    replace: [
+                        {
+                            replace: builder.id('textfield'),
+                        },
+                        {
+                            replace: builder.attribute('size', 's'),
+                        }
+                        
+                    ],
+                },
+                {
+                    find: [
+                        builder.class('spectrum-Textfield--sizeL'),
+                    ],
+                    replace: [
+                        {
+                            replace: builder.id('textfield'),
+                        },
+                        {
+                            replace: builder.attribute('size', 'l'),
+                        }
+                        
+                    ],
+                },
+                {
+                    find: [
+                        builder.class('spectrum-Textfield--sizeXL'),
+                    ],
+                    replace: [
+                        {
+                            replace: builder.id('textfield'),
+                        },
+                        {
+                            replace: builder.attribute('size', 'xl'),
+                        }
+                        
+                    ],
+                },
                 ...converter.enumerateAttributes(
                     [
                         ['spectrum-Textfield--sizeS', 's'],
