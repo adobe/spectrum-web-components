3e8d759b93c07579585955387b264ae376424cd7
Nikki Massaro <massaro@adobe.com>
2025-02-19 17:29:03 -0500

    docs(tray): added a story to test buttons in tray on mobile

--- packages/tray/stories/tray.stories.ts
+++ packages/tray/stories/tray.stories.ts
@@ -74,10 +74,14 @@ export const menu = (args: StoryArgs): TemplateResult => {
 export const buttons = (args: StoryArgs): TemplateResult => {
     return html`
         <overlay-trigger type="modal">
-            <sp-button slot="trigger" variant="secondary">Toggle menu</sp-button>
+            <sp-button slot="trigger" variant="secondary">
+                Toggle menu
+            </sp-button>
             <sp-tray slot="click-content" ?open=${args.open}>
-                <sp-button onclick="()=>alert('success!')">Can you click me?</sp-button>
+                <sp-button @click=${() => alert('success!')}>
+                    Can you click me?
+                </sp-button>
             </sp-tray>
         </overlay-trigger>
     `;
-}
+};
