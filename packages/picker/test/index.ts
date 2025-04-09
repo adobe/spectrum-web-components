11f4a7989de4a974702d1ce74671f72d5b586c87
Nikki Massaro <massaro@adobe.com>
2025-04-09 16:20:38 -0400

    test(picker): updated tests are WIP

--- packages/picker/test/index.ts
+++ packages/picker/test/index.ts
@@ -544,7 +544,7 @@ export function runPickerTests(): void {
                 'first item should not be visually focused after closing'
             ).to.be.false;
         });
-        it('opens, on click, with visible focus on a menu item', async () => {
+        it('opens, on click, with visible focus NOT on a menu item', async () => {
             await nextFrame();
             await nextFrame();
             const firstItem = el.querySelector('sp-menu-item') as MenuItem;
@@ -566,7 +566,9 @@ export function runPickerTests(): void {
             await opened;
 
             expect(el.open, 'open?').to.be.true;
-            expect(firstItem.focused, 'firstItem focused?').to.be.true;
+            expect(firstItem.focused, 'firstItem focused after click?').to.be
+                .false;
+            expect(firstItem).to.not.equal(document.activeElement);
         });
         it('opens and selects in a single pointer button interaction', async () => {
             await nextFrame();
@@ -846,7 +848,7 @@ export function runPickerTests(): void {
             expect(el.value).to.equal('');
             expect(secondItem.selected).to.be.false;
 
-            secondItem.click();
+            /*secondItem.click();
             await waitUntil(() => document.activeElement === el, 'focused', {
                 timeout: 300,
             });
@@ -854,7 +856,7 @@ export function runPickerTests(): void {
             expect(el.open, 'open?').to.be.false;
             expect(el.value, 'value changed').to.equal('option-2');
             expect(secondItem.selected, 'selected changed').to.be.true;
-            input.remove();
+            input.remove();*/
         });
         it('should throw focus after `change`', async () => {
             const input = document.createElement('input');
@@ -1369,7 +1371,7 @@ export function runPickerTests(): void {
             // we should have SAFARI_FOCUS_RING_CLASS in the classList
             expect(
                 button.classList.contains(SAFARI_FOCUS_RING_CLASS),
-                'has focus ring?'
+                'button has focus ring?'
             ).to.be.true;
 
             // picker should still have focus
@@ -1396,20 +1398,24 @@ export function runPickerTests(): void {
             });
             await opened;
 
+            expect(firstItem.focused, 'firstItem focused?').to.be.true;
+
             // Make a selection again
             closed = oneEvent(el, 'sp-closed');
-            firstItem.click();
+            await sendKeys({
+                press: 'Enter',
+            });
             await closed;
 
             await elementUpdated(el);
 
             // expect the tray to be closed
             expect(el.open, 'open?').to.be.false;
-
+            // TODO focus is not reset when  using keyboard to close
             // we should not have SAFARI_FOCUS_RING_CLASS in the classList
             expect(
                 button.classList.contains(SAFARI_FOCUS_RING_CLASS),
-                'has focus ring?'
+                'has focus ring again?'
             ).to.be.false;
         });
         it('does not close on document scroll', async () => {
