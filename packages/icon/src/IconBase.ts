7757a68ac8e5d5d2af5150bdb278a1f655ea794b
Rajdeep Chandra <rajdeepchandra@Rajdeeps-MacBook-Pro-2.local>
2025-05-06 10:49:48 +0530

    chore: modularize lint staged config

--- packages/icon/src/IconBase.ts
+++ packages/icon/src/IconBase.ts
@@ -33,6 +33,7 @@ export class IconBase extends SpectrumElement {
     public static override get styles(): CSSResultArray {
         return [iconStyles];
     }
+
     private unsubscribeSystemContext: (() => void) | null = null;
 
     @state()
