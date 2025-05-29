e0b7ba5615ac3a1acf2a364c5fb637c3bf52a213
castastrophe <castastrophe@users.noreply.github.com>
2025-05-29 14:31:00 -0400

    fix(tooltip,textfield,sidenav): update deprecated css; copyright formatting throughout

--- test/plugins/send-mouse-plugin.ts
+++ test/plugins/send-mouse-plugin.ts
@@ -1,14 +1,14 @@
-/*
-Copyright 2020 Adobe. All rights reserved.
-This file is licensed to you under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License. You may obtain a copy
-of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
-OF ANY KIND, either express or implied. See the License for the specific language
-governing permissions and limitations under the License.
-*/
+/**
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 import type { Page } from 'playwright';
 
 export type Step = {
@@ -31,7 +31,7 @@ export function sendMousePlugin() {
                 id: string;
                 browser: { type: string; getPage: (id: string) => Page };
             };
-        }): Promise<any> {
+        }): Promise<boolean> {
             if (command === 'send-pointer') {
                 // handle specific behavior for playwright
                 if (session.browser.type === 'playwright') {
@@ -60,6 +60,7 @@ export function sendMousePlugin() {
                     `Sending mouse commands is not supported for browser type ${session.browser.type}.`
                 );
             }
+            throw new Error(`Unsupported command: ${command}`);
         },
     };
 }
