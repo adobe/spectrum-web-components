45d44ddaba6ebf690505db4ec2056a8110099356
Ruben Carvalho <rcarvalho@adobe.com>
2025-06-10 10:42:39 +0200

    chore: fix test imports

--- swan/src/components/badge/badge.test.ts
+++ swan/src/components/badge/badge.test.ts
@@ -11,7 +11,7 @@ governing permissions and limitations under the License.
 */
 
 import { describe, it, expect } from 'vitest';
-import { BADGE_VARIANTS, FIXED_VALUES } from './badge.component.js';
+import { BADGE_VARIANTS, FIXED_VALUES } from './badge.base.js';
 
 describe('SwanBadge - Unit Tests', () => {
     it('should export all badge variants', () => {
