0a753820881e5581bd7c3f2da18d21b46d192262
Casey Eickhoff <ceickhoff@adobe.com>
2025-07-17 17:27:40 -0600

    chore: remove unnecessary import

--- packages/number-field/stories/number-field.stories.ts
+++ packages/number-field/stories/number-field.stories.ts
@@ -17,7 +17,6 @@ import '@spectrum-web-components/number-field/sp-number-field.js';
 import '@spectrum-web-components/field-label/sp-field-label.js';
 import { spreadProps } from '../../../test/lit-helpers.js';
 import { NumberField } from '@spectrum-web-components/number-field/src/NumberField.js';
-import '../../../tools/theme/sp-theme';
 
 export default {
     title: 'Number Field',
