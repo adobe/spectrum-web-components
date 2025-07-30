667945423e5dfad9c01f652c4fa8a300a7763267
Nikki Massaro <massaro@adobe.com>
2025-07-30 14:14:53 -0400

    chore(progress-circle): renamed for consistency with badge's base

--- packages/progress-circle/src/ProgressCircle.ts
+++ packages/progress-circle/src/ProgressCircle.ts
@@ -16,7 +16,7 @@ import {
     TemplateResult,
 } from '@spectrum-web-components/base';
 import { ifDefined } from '@spectrum-web-components/base/src/directives.js';
-import { ProgressCircleBase } from './ProgressCircleBase.js';
+import { ProgressCircleBase } from './ProgressCircle.base.js';
 
 import progressCircleStyles from './progress-circle.css.js';
 
