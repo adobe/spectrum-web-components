d43e6ec42c7dd92e99b866d361348b3d85115a87
Nikki Massaro <massaro@adobe.com>
2025-08-25 14:54:42 -0400

    chore: updated regex for inline bulleted items only

--- scripts/escape-changelog-tags.js
+++ scripts/escape-changelog-tags.js
@@ -3,7 +3,7 @@ import { replaceInFile } from 'replace-in-file';
 // make sure inline tags are escaped
 const tagOptions = {
     files: '.changeset/*.md',
-    from: /`?(<[a-zA-Z]+(-[a-zA-Z]+)*>)`?/g,
+    from: /(?<=\n\s*-\s.*)`?(<\w+(-\w+)*[^>]*>)`?/g,
     to: '`$1`',
 };
 
@@ -13,21 +13,6 @@ try {
 } catch (error) {
     console.error(
         'Error occurred replacing unescaped tags in .changeset/*.md:',
-        error
-    );
-}
-//make sure we don't escape tags in code snippets
-const codeOptions = {
-    files: '.changeset/*.md',
-    from: /(`{3,})([A-Za-z]{0,4}\n)([^`]*)`(<\w+(?:-\w+)*>)`([^`]*)/g, // (`{3,})([A-Za-z]{0,4}\n)([^`]*)`(<\w+(-\w+)*>)`{1}
-    to: '$1$2$3$4$5',
-};
-try {
-    const results = await replaceInFile(codeOptions);
-    console.log('Do not escape code snippet tags in .changeset/*.md:', results);
-} catch (error) {
-    console.error(
-        'Error occurred avoiding code snippet tags in .changeset/*.md:',
         error
     );
 }
