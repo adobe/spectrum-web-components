752a7040bd1fc1fbd011d4801ca480f2f6fbbca7
Ruben Carvalho <rcarvalho@adobe.com>
2025-09-01 16:40:46 +0200

    chore: fix license.js

--- first-gen/config/license.js
+++ first-gen/config/license.js
@@ -1,4 +1,4 @@
-module.exports = `/**
+/**
  * Copyright 2025 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
@@ -8,4 +8,4 @@ module.exports = `/**
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
- */`;
+ */
