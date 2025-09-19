9a2da7843395fe281e6cca9ea169e2b0541f7313
Ruben Carvalho <rcarvalho@adobe.com>
2025-09-19 15:52:07 +0200

    chore: add minimum vitest addon setup (WIP)

--- second-gen/packages/swc/.storybook/main.ts
+++ second-gen/packages/swc/.storybook/main.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 const config = {
     stories: ['../components/**/*.stories.@(js|ts|md|mdx)'],
     framework: '@storybook/web-components-vite',
+    addons: ['@storybook/addon-vitest'],
     core: {
         disableTelemetry: true,
     },
