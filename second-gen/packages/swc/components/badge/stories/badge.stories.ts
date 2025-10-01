850a0bd579df3d577086871d3217f3cb19ebf5f3
Gray Norton <gnorton@adobe.com>
2025-09-30 21:39:26 -0700

    chore: update badge to match barebones patterns

--- second-gen/packages/swc/components/badge/stories/badge.stories.ts
+++ second-gen/packages/swc/components/badge/stories/badge.stories.ts
@@ -10,41 +10,45 @@
  * governing permissions and limitations under the License.
  */
 
-import { html, nothing, TemplateResult } from 'lit';
+import { html, TemplateResult } from 'lit';
 import { styleMap } from 'lit/directives/style-map.js';
-import type { Meta, StoryObj } from '@storybook/web-components';
+import type { Meta, StoryObj as Story } from '@storybook/web-components';
+import { getStorybookHelpers } from '@wc-toolkit/storybook-helpers';
 
-import {
-    BADGE_VARIANTS,
-    BADGE_VARIANTS_COLOR,
-    BADGE_VARIANTS_SEMANTIC,
-    FIXED_VALUES,
-} from '@swc/components/badge';
+import { Badge } from '@swc/components/badge';
 
 import '@swc/components/badge';
 
-/** @todo Pull this up into a utility function for all components to leverage */
-function capitalize(str?: string): string {
-    if (typeof str !== 'string') {
-        return '';
-    }
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+// ────────────────
+//    METADATA
+// ────────────────
 
-/** @todo Pull this up into a decorator for all stories to leverage */
-const CONTAINER = (content: TemplateResult<1>[]) =>
-    html`<div
-        style=${styleMap({
-            display: 'flex',
-            gap: 'var(--spectrum-spacing-200)',
-            'flex-wrap': 'wrap',
-            'justify-content': 'center',
-            // Used 80ch because that's generally considered the maximum readable width for text in a web page.
-            'max-inline-size': '80ch',
-        })}
-    >
-        ${content}
-    </div>`;
+const { events, args, argTypes, template } = getStorybookHelpers('swc-badge');
+
+argTypes.variant = {
+    ...argTypes.variant,
+    control: { type: 'select' },
+    options: Badge.VARIANTS,
+};
+
+argTypes.fixed = {
+    ...argTypes.fixed,
+    control: { type: 'select' },
+    options: [undefined, ...Badge.FIXED_VALUES],
+};
+
+/*
+ * @todo This is properly configuring the Select, but the control doesn't
+ * seem to work; need to investigate.
+ */
+
+// argTypes.size = {
+//     ...argTypes.size,
+//     control: { type: 'select' },
+//     options: Badge.VALID_SIZES,
+// };
+
+args['default-slot'] = 'Badge';
 
 /**
  * Badges are for showing a small amount of color-categorized metadata. They're ideal for getting a user's attention. There are two additional styles - subtle fill and outline - in addition to the default, bold fill style.
@@ -54,80 +58,25 @@ const CONTAINER = (content: TemplateResult<1>[]) =>
 const meta: Meta = {
     title: 'Components/Badge',
     component: 'swc-badge',
-    argTypes: {
-        size: {
-            name: 'Size',
-            control: { type: 'select' },
-            options: ['s', 'm', 'l', 'xl'],
-        },
-        variant: {
-            name: 'Variant',
-            control: { type: 'select' },
-            options: BADGE_VARIANTS,
-        },
-        subtle: {
-            name: 'Subtle',
-            control: { type: 'boolean' },
-        },
-        outline: {
-            name: 'Outline',
-            control: { type: 'boolean' },
-        },
-        fixed: {
-            name: 'Fixed',
-            control: { type: 'select' },
-            options: [...FIXED_VALUES],
-        },
-        label: {
-            name: 'Label',
-            control: { type: 'text' },
-            table: { category: 'Slots' },
-        },
-        icon: {
-            name: 'Icon',
-            control: { type: 'text' },
-            table: { category: 'Slots' },
-        },
-    },
-    args: {
-        label: 'Badge',
-        // updating this to make the options more readable as a demo
-        size: 'l',
-        variant: 'informative',
-        subtle: false,
-        outline: false,
-    },
+    args,
+    argTypes,
+    render: (args) => template(args),
     parameters: {
-        design: {
-            type: 'figma',
-            url: 'https://www.figma.com/design/Mngz9H7WZLbrCvGQf3GnsY/S2-%2F-Desktop?node-id=36806-6551',
+        actions: {
+            handles: events,
         },
     },
     tags: ['migrated'],
 };
 
 export default meta;
-type Story = StoryObj;
-type StoryArgs = StoryObj['args'];
 
-/**
- * @param args - The arguments for the badge as sourced from the storybook args.
- * @returns A lightweight template for to render a single badge with bindings to the args.
- * @todo could also surface a knob to select an icon from our icon library and use that instead of the string.
- * @todo is there stronger typing we can use for the args?
- */
-const BASE_TEMPLATE = (args: StoryArgs = {}) => html`
-    <swc-badge
-        variant="${args.variant}"
-        size="${args.size || 'm'}"
-        .fixed=${args.fixed}
-        .subtle=${args.subtle}
-        .outline=${args.outline}
-    >
-        ${args.icon ? html`<span slot="icon">${args.icon}</span>` : nothing}
-        ${args.label}
-    </swc-badge>
-`;
+// ───────────────
+//    STORIES
+// ───────────────
+
+type BadgeVariant = typeof Badge.prototype.variant;
+type BadgeSize = typeof Badge.prototype.size;
 
 /**
  * Badges can contain label, icon, or label and icon. Text wrapping is also included when a `max-inline-size` is applied to the badge.
@@ -136,7 +85,6 @@ export const Default: Story = {
     args: {
         size: 'm',
     },
-    render: BASE_TEMPLATE,
 };
 
 /**
@@ -144,9 +92,8 @@ export const Default: Story = {
  */
 export const WithIcon: Story = {
     args: {
-        icon: '✓',
+        ['icon-slot']: '✓',
     },
-    render: BASE_TEMPLATE,
     // Removes the story from the side navigation while keeping in the docs view
     tags: ['!dev'],
 };
@@ -155,14 +102,14 @@ export const WithIcon: Story = {
  * Semantic variants allow you to render the badge with a descriptive name that maps to a design-system-aligned color. This is the preferred way to assign color to a badge because it will align more consistently with other components in your UI with the same meaning.
  */
 export const SemanticVariants: Story = {
-    render: (args) =>
+    render: () =>
         CONTAINER(
-            BADGE_VARIANTS_SEMANTIC.map((variant) =>
-                BASE_TEMPLATE({
-                    ...args,
-                    variant,
-                    label: capitalize(variant),
-                })
+            Badge.VARIANTS_SEMANTIC.map(
+                (variant) => html`
+                    <swc-badge variant=${variant as BadgeVariant}
+                        >${capitalize(variant)}</swc-badge
+                    >
+                `
             )
         ),
     tags: ['!dev'],
@@ -175,18 +122,17 @@ export const Outline: Story = {
     argTypes: {
         variant: {
             control: { type: 'select' },
-            options: BADGE_VARIANTS_SEMANTIC,
+            options: Badge.VARIANTS_SEMANTIC,
         },
     },
-    render: (args) =>
+    render: () =>
         CONTAINER(
-            BADGE_VARIANTS_SEMANTIC.map((variant) =>
-                BASE_TEMPLATE({
-                    ...args,
-                    variant,
-                    label: capitalize(variant),
-                    outline: true,
-                })
+            Badge.VARIANTS_SEMANTIC.map(
+                (variant) => html`
+                    <swc-badge variant=${variant as BadgeVariant} outline
+                        >${capitalize(variant)}</swc-badge
+                    >
+                `
             )
         ),
     tags: ['!dev'],
@@ -196,28 +142,28 @@ export const Outline: Story = {
  * Color variants are available for the badge component and provide a more granular access to the full color palette in the design system.
  */
 export const ColorVariants: Story = {
-    render: (args) =>
+    render: () =>
         CONTAINER(
-            BADGE_VARIANTS_COLOR.map((variant) =>
-                BASE_TEMPLATE({
-                    ...args,
-                    variant,
-                    label: capitalize(variant),
-                })
+            Badge.VARIANTS_COLOR.map(
+                (variant) => html`
+                    <swc-badge variant=${variant as BadgeVariant}
+                        >${capitalize(variant)}</swc-badge
+                    >
+                `
             )
         ),
     tags: ['!dev'],
 };
 
 export const Sizes: Story = {
-    render: (args) =>
+    render: () =>
         CONTAINER(
-            ['s', 'm', 'l', 'xl'].map((size) =>
-                BASE_TEMPLATE({
-                    ...args,
-                    size,
-                    label: capitalize(size),
-                })
+            Badge.VALID_SIZES.map(
+                (size) => html`
+                    <swc-badge size=${size as BadgeSize}
+                        >${capitalize(size)}</swc-badge
+                    >
+                `
             )
         ),
     tags: ['!dev'],
@@ -227,16 +173,43 @@ export const Sizes: Story = {
  * The `subtle` style is available for all variants. It is useful when you want to reduce the visual prominence of the badge while still mapping to the design system color palette.
  */
 export const Subtle: Story = {
-    render: (args) =>
+    render: () =>
         CONTAINER(
-            BADGE_VARIANTS.map((variant) =>
-                BASE_TEMPLATE({
-                    ...args,
-                    variant,
-                    label: capitalize(variant),
-                    subtle: true,
-                })
+            Badge.VARIANTS.map(
+                (variant) => html`
+                    <swc-badge variant=${variant as BadgeVariant} subtle
+                        >${capitalize(variant)}</swc-badge
+                    >
+                `
             )
         ),
     tags: ['!dev'],
 };
+
+// ────────────────────────
+//    HELPER FUNCTIONS
+// ────────────────────────
+
+/* @todo Pull this up into a utility function for all components to leverage */
+function capitalize(str?: string): string {
+    if (typeof str !== 'string') {
+        return '';
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+/* @todo Pull this up into a decorator for all stories to leverage */
+function CONTAINER(content: TemplateResult<1>[]): TemplateResult {
+    return html`<div
+        style=${styleMap({
+            display: 'flex',
+            gap: 'var(--spectrum-spacing-200)',
+            'flex-wrap': 'wrap',
+            'justify-content': 'center',
+            // Used 80ch because that's generally considered the maximum readable width for text in a web page.
+            'max-inline-size': '80ch',
+        })}
+    >
+        ${content}
+    </div>`;
+}
