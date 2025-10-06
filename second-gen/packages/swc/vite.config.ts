047f5393518b4743741fb1b5bd51b4886eed6f48
Ruben Carvalho <rcarvalho@adobe.com>
2025-10-06 14:56:32 +0200

    chore: don't bundle lit dependencies

--- second-gen/packages/swc/vite.config.ts
+++ second-gen/packages/swc/vite.config.ts
@@ -65,7 +65,12 @@ export default defineConfig({
         },
         rollupOptions: {
             external: (id) => {
-                return id === 'lit' || id.startsWith('@swc/core/');
+                return (
+                    id === 'lit' ||
+                    id.startsWith('lit/') ||
+                    id.startsWith('@lit/') ||
+                    id.startsWith('@swc/core/')
+                );
             },
             output: {
                 preserveModules: true,
