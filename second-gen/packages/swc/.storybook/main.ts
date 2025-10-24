9eccf1f43b6a60dcf4ad807624f8f5b3597aeaa1
Casey Eickhoff <ceickhoff@adobe.com>
2025-10-24 15:11:35 -0600

    chore: linting fixes and dirname fix for second-gen

--- second-gen/packages/swc/.storybook/main.ts
+++ second-gen/packages/swc/.storybook/main.ts
@@ -1,4 +1,8 @@
-import { resolve } from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
