5de5934317a2c005b1c044dd70f51f007cb1a11c
Ruben Carvalho <rcarvalho@adobe.com>
2025-10-29 23:37:12 +0100

    chore: update Storybook to v10

--- 2nd-gen/packages/swc/.storybook/main.ts
+++ 2nd-gen/packages/swc/.storybook/main.ts
@@ -1,4 +1,7 @@
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 const config = {
