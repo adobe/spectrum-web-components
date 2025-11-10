d79623779c7a04d4780382a059a7a6a0bec2f2cc
Helen Le <helenl@adobe.com>
2025-11-10 13:53:37 -0800

    fix(shared): patch focusable selectors

--- tools/shared/src/focusable-selectors.ts
+++ tools/shared/src/focusable-selectors.ts
@@ -11,19 +11,14 @@
  */
 
 const focusables = [
-    'input:not([inert])',
-    'select:not([inert])',
-    'textarea:not([inert])',
-    'a[href]:not([inert])',
-    'button:not([inert])',
-    'label:not([inert])',
-    '[tabindex]:not([inert])',
-    'audio[controls]:not([inert])',
-    'video[controls]:not([inert])',
-    '[contenteditable]:not([contenteditable="false"]):not([inert])',
-    'details>summary:first-of-type:not([inert])',
-    'details:not([inert])',
-    '[focusable]:not([focusable="false"])', // custom dev use-case
+    'button',
+    '[focusable]',
+    '[href]',
+    'input',
+    'label',
+    'select',
+    'textarea',
+    '[tabindex]',
 ];
 
 const userFocuable = ':not([tabindex="-1"])';
