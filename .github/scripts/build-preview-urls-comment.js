693a62e40ee95e91368e91ba6a93aed2ca3e6e03
Casey Eickhoff <ceickhoff@adobe.com>
2025-11-11 11:12:15 -0700

    Revert "chore: publish 2nd generation preview docs in PR"
    This reverts commit 560194f8b4dae70ce0c619fbc76974a79b37757d.

--- .github/scripts/build-preview-urls-comment.js
+++ .github/scripts/build-preview-urls-comment.js
@@ -82,17 +82,15 @@ export const buildPreviewURLComment = (prNumber) => {
 - [High Contrast Mode | Medium | LTR](${hcmUrl})`);
 
     // Documentation and Storybook URLs
-    const docs1stGenUrl = `${baseUrl}/${prHash}/docs/1st-gen-docs/`;
-    const storybook1stGenUrl = `${baseUrl}/${prHash}/docs/1st-gen-storybook/`;
-    const storybook2ndGenUrl = `${baseUrl}/${prHash}/docs/2nd-gen-storybook/`;
+    const docsUrl = `${baseUrl}/${prHash}/docs/`;
+    const storybookUrl = `${baseUrl}/${prHash}/docs/storybook/`;
 
-    let comment = `## 📚 Branch Preview Links
+    let comment = `## 📚 Branch Preview
 
-- [Documentation Site (1st-gen)](${docs1stGenUrl})
-- [Storybook (1st-gen)](${storybook1stGenUrl})
-- [Storybook (2nd-gen)](${storybook2ndGenUrl})
+- [Documentation Site](${docsUrl})
+- [Storybook](${storybookUrl})
 
-<h3><strong>🔍 First GenerationVisual Regression Test Results</strong></h3>
+<h3><strong>🔍 Visual Regression Test Results</strong></h3>
 
 When a visual regression test fails (or has previously failed while working on this branch), its results can be found in the following URLs:
 
