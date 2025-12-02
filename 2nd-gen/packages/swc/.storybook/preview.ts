4fba00fc2254bd1da3a5cd450491e14924bd30e8
Casey Eickhoff <48574582+caseyisonit@users.noreply.github.com>
2025-12-02 13:40:20 -0700

    initial stubbing (#5915)
    * chore: two ways of working with markdown files for guides
    
    * chore: cursor rule for migrating markdown to mdx if needed
    
    * chore: stubbed docs and ordered them
    
    * chore: lint fix

--- 2nd-gen/packages/swc/.storybook/preview.ts
+++ 2nd-gen/packages/swc/.storybook/preview.ts
@@ -1,8 +1,9 @@
 /** @type { import('@storybook/web-components').Preview } */
+import '../tokens/global-vars.css';
 import '../tokens/index.css';
 import '../tokens/light-vars.css';
 import '../tokens/medium-vars.css';
-import '../tokens/global-vars.css';
+import DocumentTemplate from './DocumentTemplate.mdx';
 
 import { setCustomElementsManifest } from '@storybook/web-components';
 import {
@@ -46,6 +47,44 @@ const preview = {
             config: {
                 rules: [
                     // Customize axe-core rules if needed
+                ],
+            },
+        },
+        docs: {
+            codePanel: true,
+            page: DocumentTemplate,
+            toc: {
+                contentsSelector: '.sbdocs-content',
+                headingSelector: 'h2, h3',
+                ignoreSelector: '#primary',
+                disable: false,
+                unsafeTocbotOptions: {
+                    orderedList: false,
+                },
+            },
+        },
+        options: {
+            storySort: {
+                method: 'alphabetical-by-kind',
+                order: [
+                    'Get Started',
+                    [
+                        'Welcome to 2nd-gen SWC',
+                        'What is SWC?',
+                        'When to use SWC?',
+                        'First Gen vs Second Gen',
+                    ],
+                    'Components',
+                    'Guides',
+                    [
+                        'Getting started guide',
+                        'Contributor guide',
+                        'Style guide',
+                        'Project planning',
+                        'Accessibility guides',
+                        'React wrappers',
+                    ],
+                    'Resources',
                 ],
             },
         },
