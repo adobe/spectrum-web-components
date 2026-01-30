8c0875d778bfdada3c1c5875860cbb571c0848e8
Ruben Carvalho <rcarvalho@adobe.com>
2026-01-30 10:01:58 +0100

    chore: revert safe wildcard exports

--- 1st-gen/tools/base/src/index.ts
+++ 1st-gen/tools/base/src/index.ts
@@ -10,13 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-export { SpectrumElement, SpectrumMixin } from './Base.js';
-export type { SpectrumInterface } from './Base.js';
-export { ElementSizes, SizedMixin } from './sizedMixin.js';
-export type {
-    DefaultElementSize,
-    ElementSize,
-    SizedElementInterface,
-} from './sizedMixin.js';
-export { INPUT_COMPONENT_TAGS, INPUT_COMPONENT_PATTERN } from './constants.js';
+export * from './Base.js';
+export * from './sizedMixin.js';
+export * from './constants.js';
 export * from 'lit';
