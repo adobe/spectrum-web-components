ea7b95bc9e435758da7a1f08b89c4bb894b3dba6
TarunAdobe <ttomar@adobe.com>
2026-02-06 14:47:41 +0530

    chore: migrate icon to core and second-gen

---
+++ 2nd-gen/packages/swc/components/icon/Icon.ts
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2026 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { CSSResultArray, html, TemplateResult } from 'lit';
+
+import { IconBase } from '@spectrum-web-components/core/components/icon';
+
+import styles from './icon.css';
+
+/**
+ * Minimal icon renderer that accepts a slotted SVG or an image source URL.
+ *
+ * @element swc-icon
+ *
+ * @example
+ * <swc-icon src="https://example.com/icon.svg" label="Search"></swc-icon>
+ *
+ * @example
+ * <swc-icon label="Search">
+ *   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+ *     <path d="M14.5 13.09 11.41 10a6 6 0 1 0-1.41 1.41l3.09 3.09a1 1 0 0 0 1.41-1.41zM3 7a4 4 0 1 1 8 0 4 4 0 0 1-8 0z"/>
+ *   </svg>
+ * </swc-icon>
+ */
+export class Icon extends IconBase {
+    // ──────────────────────────────
+    //     RENDERING & STYLING
+    // ──────────────────────────────
+
+    public static override get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    protected override render(): TemplateResult {
+        if (this.src) {
+            return html`
+                <img class="swc-Icon" src=${this.src} alt=${this.label} />
+            `;
+        }
+        return html`
+            <span class="swc-Icon">
+                <slot @slotchange=${this.handleSlotChange}></slot>
+            </span>
+        `;
+    }
+}
