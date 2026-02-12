1ae945f7dd8dd9389555918f4bf9c227f41b5b11
Rajdeep Chandra <rajdeepc@adobe.com>
2026-02-12 11:51:01 +0530

    fix: slot added to their canonical locations

--- 2nd-gen/packages/core/components/badge/Badge.base.ts
+++ 2nd-gen/packages/core/components/badge/Badge.base.ts
@@ -12,12 +12,12 @@
 import { PropertyValues } from 'lit';
 import { property } from 'lit/decorators.js';
 
+import { ObserveSlotPresence } from '@spectrum-web-components/core/mixins/observe-slot-presence.js';
+import { ObserveSlotText } from '@spectrum-web-components/core/mixins/observe-slot-text.js';
 import {
     SizedMixin,
     SpectrumElement,
 } from '@spectrum-web-components/core/shared/base/index.js';
-import { ObserveSlotPresence } from '@spectrum-web-components/core/shared/observe-slot-presence.js';
-import { ObserveSlotText } from '@spectrum-web-components/core/shared/observe-slot-text.js';
 
 import {
     BADGE_VARIANTS_SEMANTIC,
