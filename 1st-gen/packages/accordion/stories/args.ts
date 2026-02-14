8a9c52812e14946c71f9c92721a856ee83cef2ef
Ruben Carvalho <rcarvalho@adobe.com>
2026-02-14 23:56:55 +0100

    chore: revert accordion format apply

--- 1st-gen/packages/accordion/stories/args.ts
+++ 1st-gen/packages/accordion/stories/args.ts
@@ -11,78 +11,78 @@
  */
 
 export const argTypes = {
-  open: {
-    name: 'open',
-    type: { name: 'boolean', required: false },
-    description: 'Whether the second accordion item is open.',
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: false },
+    open: {
+        name: 'open',
+        type: { name: 'boolean', required: false },
+        description: 'Whether the second accordion item is open.',
+        table: {
+            type: { summary: 'boolean' },
+            defaultValue: { summary: false },
+        },
+        control: {
+            type: 'boolean',
+        },
     },
-    control: {
-      type: 'boolean',
+    allowMultiple: {
+        name: 'allowMultiple',
+        type: { name: 'boolean', required: false },
+        description:
+            'Whether multipel Accordion Items can be open at the same time.',
+        table: {
+            type: { summary: 'boolean' },
+            defaultValue: { summary: false },
+        },
+        control: {
+            type: 'boolean',
+        },
     },
-  },
-  allowMultiple: {
-    name: 'allowMultiple',
-    type: { name: 'boolean', required: false },
-    description:
-      'Whether multipel Accordion Items can be open at the same time.',
-    table: {
-      type: { summary: 'boolean' },
-      defaultValue: { summary: false },
+    density: {
+        name: 'density',
+        type: { name: 'string', required: false },
+        description: 'The density at which to display accordion items.',
+        table: {
+            defaultValue: { summary: 'default' },
+        },
+        control: {
+            labels: {
+                compact: 'Compact',
+                spacious: 'Spacious',
+                default: 'Default',
+            },
+            type: 'select',
+        },
     },
-    control: {
-      type: 'boolean',
+    size: {
+        name: 'size',
+        type: { name: 'string', required: false },
+        description: 'The size at which to display accordion items.',
+        table: {
+            defaultValue: { summary: 'm' },
+        },
+        control: {
+            labels: {
+                s: 'Small',
+                m: 'Medium',
+                l: 'Large',
+                xl: 'Extra large',
+            },
+            type: 'select',
+        },
     },
-  },
-  density: {
-    name: 'density',
-    type: { name: 'string', required: false },
-    description: 'The density at which to display accordion items.',
-    table: {
-      defaultValue: { summary: 'default' },
+    level: {
+        name: 'level',
+        type: { name: 'number', required: false },
+        description:
+            'The heading level (2-6) to use for all accordion item titles.',
+        table: {
+            type: { summary: 'number' },
+            defaultValue: { summary: 3 },
+        },
+        control: {
+            type: 'number',
+            min: 2,
+            max: 6,
+            step: 1,
+        },
     },
-    control: {
-      labels: {
-        compact: 'Compact',
-        spacious: 'Spacious',
-        default: 'Default',
-      },
-      type: 'select',
-    },
-  },
-  size: {
-    name: 'size',
-    type: { name: 'string', required: false },
-    description: 'The size at which to display accordion items.',
-    table: {
-      defaultValue: { summary: 'm' },
-    },
-    control: {
-      labels: {
-        s: 'Small',
-        m: 'Medium',
-        l: 'Large',
-        xl: 'Extra large',
-      },
-      type: 'select',
-    },
-  },
-  level: {
-    name: 'level',
-    type: { name: 'number', required: false },
-    description:
-      'The heading level (2-6) to use for all accordion item titles.',
-    table: {
-      type: { summary: 'number' },
-      defaultValue: { summary: 3 },
-    },
-    control: {
-      type: 'number',
-      min: 2,
-      max: 6,
-      step: 1,
-    },
-  },
 };
