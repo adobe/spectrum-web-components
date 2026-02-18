14d340e54ed91b571a7fcb73e3476b7a1c998e9d
5t3ph <thinkdobecreate@gmail.com>
2026-02-18 11:44:07 -0600

    fix(*): prettier

--- 2nd-gen/packages/swc/components/typography/stories/typography.stories.ts
+++ 2nd-gen/packages/swc/components/typography/stories/typography.stories.ts
@@ -14,11 +14,11 @@ import { html } from 'lit';
 import type { Meta, StoryObj as Story } from '@storybook/web-components';
 
 import {
-    LANGS,
-    SIZES,
-    template,
-    type TypographyTemplateProps,
-    VARIANTS,
+  LANGS,
+  SIZES,
+  template,
+  type TypographyTemplateProps,
+  VARIANTS,
 } from './typography.template.js';
 
 /**
@@ -29,56 +29,58 @@ import {
  * Learn more about [loading the expected fonts](/?path=/docs/guides-customization-fonts--readme).
  */
 const meta: Meta<TypographyTemplateProps> = {
-    title: 'Typography',
-    parameters: {
-        docs: {
-            subtitle: `Typography variant styles are available via CSS classes.`,
-            canvas: {
-                sourceState: 'hidden',
-            },
-        },
+  title: 'Typography',
+  parameters: {
+    docs: {
+      subtitle: `Typography variant styles are available via CSS classes.`,
+      canvas: {
+        sourceState: 'hidden',
+      },
     },
-    argTypes: {
-        variant: { control: 'select', options: VARIANTS },
-        size: { control: 'select', options: SIZES },
-        serif: { control: 'boolean' },
-        heavy: { control: 'boolean' },
-        emphasized: { control: 'boolean' },
-        margins: { control: 'boolean' },
-        prose: { control: 'boolean' },
-        lang: { control: 'select', options: LANGS },
-        includeMultipleSizes: { control: 'boolean' },
-        showAllVariants: { control: 'boolean' },
-        sampleText: { control: 'text' },
-    },
-    render: (args) => html` ${template(args)} `,
-    tags: ['migrated'],
+  },
+  argTypes: {
+    variant: { control: 'select', options: VARIANTS },
+    size: { control: 'select', options: SIZES },
+    serif: { control: 'boolean' },
+    heavy: { control: 'boolean' },
+    emphasized: { control: 'boolean' },
+    margins: { control: 'boolean' },
+    prose: { control: 'boolean' },
+    lang: { control: 'select', options: LANGS },
+    includeMultipleSizes: { control: 'boolean' },
+    showAllVariants: { control: 'boolean' },
+    sampleText: { control: 'text' },
+  },
+  render: (args) => html`
+    ${template(args)}
+  `,
+  tags: ['migrated'],
 };
 
 export default meta;
 
 export const Playground: Story = {
-    args: {
-        variant: 'heading',
-        size: 'M',
-        serif: false,
-        heavy: false,
-        emphasized: false,
-        margins: false,
-        prose: false,
-        lang: undefined,
-        includeMultipleSizes: false,
-        showAllVariants: false,
-        sampleText: '',
+  args: {
+    variant: 'heading',
+    size: 'M',
+    serif: false,
+    heavy: false,
+    emphasized: false,
+    margins: false,
+    prose: false,
+    lang: undefined,
+    includeMultipleSizes: false,
+    showAllVariants: false,
+    sampleText: '',
+  },
+  parameters: {
+    docs: {
+      canvas: {
+        sourceState: 'shown',
+      },
     },
-    parameters: {
-        docs: {
-            canvas: {
-                sourceState: 'shown',
-            },
-        },
-    },
-    tags: ['autodocs', 'dev'],
+  },
+  tags: ['autodocs', 'dev'],
 };
 
 /**
@@ -87,23 +89,23 @@ export const Playground: Story = {
  * Each type variant defaults to sans-serif and size medium.
  */
 export const Defaults: Story = {
-    args: {
-        showAllVariants: true,
-    },
-    parameters: { 'section-order': 1 },
-    tags: ['options'],
+  args: {
+    showAllVariants: true,
+  },
+  parameters: { 'section-order': 1 },
+  tags: ['options'],
 };
 
 /**
  * The following variants include a serif sub-variant, which can be used by adding the `--serif` modifier class alongside the base class.
  */
 export const SerifModifier: Story = {
-    args: {
-        showAllVariants: true,
-        serif: true,
-    },
-    parameters: { 'section-order': 2 },
-    tags: ['options'],
+  args: {
+    showAllVariants: true,
+    serif: true,
+  },
+  parameters: { 'section-order': 2 },
+  tags: ['options'],
 };
 
 /**
@@ -111,26 +113,26 @@ export const SerifModifier: Story = {
  * It may also be applied to the serif sub-variants.
  */
 export const EmphasizedModifier: Story = {
-    args: {
-        showAllVariants: true,
-        emphasized: true,
-    },
-    parameters: { 'section-order': 3 },
-    tags: ['options'],
+  args: {
+    showAllVariants: true,
+    emphasized: true,
+  },
+  parameters: { 'section-order': 3 },
+  tags: ['options'],
 };
 
 /**
  * Heading text represents the biggest and boldest text on a page, and it draws the most attention. Only the broadest idea, such as the main page title, should use this style.
  */
 export const HeadingVariant: Story = {
-    args: {
-        variant: 'heading',
-        includeMultipleSizes: true,
-    },
-    parameters: {
-        'section-order': 4,
-    },
-    tags: ['options'],
+  args: {
+    variant: 'heading',
+    includeMultipleSizes: true,
+  },
+  parameters: {
+    'section-order': 4,
+  },
+  tags: ['options'],
 };
 
 /**
@@ -138,14 +140,14 @@ export const HeadingVariant: Story = {
  * While the Heading style is for the loudest, most broad message, there are still going to be other important items in an information hierarchy.
  */
 export const TitleVariant: Story = {
-    args: {
-        variant: 'title',
-        includeMultipleSizes: true,
-    },
-    parameters: {
-        'section-order': 5,
-    },
-    tags: ['options'],
+  args: {
+    variant: 'title',
+    includeMultipleSizes: true,
+  },
+  parameters: {
+    'section-order': 5,
+  },
+  tags: ['options'],
 };
 
 /**
@@ -153,12 +155,12 @@ export const TitleVariant: Story = {
  * It's used for the text that creates the main content on a page.
  */
 export const BodyVariant: Story = {
-    args: {
-        variant: 'body',
-        includeMultipleSizes: true,
-    },
-    parameters: { 'section-order': 6 },
-    tags: ['options'],
+  args: {
+    variant: 'body',
+    includeMultipleSizes: true,
+  },
+  parameters: { 'section-order': 6 },
+  tags: ['options'],
 };
 
 /**
@@ -166,34 +168,34 @@ export const BodyVariant: Story = {
  * Text using the Detail style acts as supporting context to any other information presented, such as metadata, helper copy, or captions.
  */
 export const DetailVariant: Story = {
-    args: {
-        variant: 'detail',
-        includeMultipleSizes: true,
-    },
-    parameters: { 'section-order': 7 },
-    tags: ['options'],
+  args: {
+    variant: 'detail',
+    includeMultipleSizes: true,
+  },
+  parameters: { 'section-order': 7 },
+  tags: ['options'],
 };
 
 export const CodeVariant: Story = {
-    args: {
-        variant: 'code',
-        includeMultipleSizes: true,
-    },
-    parameters: { 'section-order': 8 },
-    tags: ['options'],
+  args: {
+    variant: 'code',
+    includeMultipleSizes: true,
+  },
+  parameters: { 'section-order': 8 },
+  tags: ['options'],
 };
 
 /**
  * Applies block-direction margins via the `--margins` modifier.
  */
 export const MarginsModifier: Story = {
-    args: {
-        variant: 'body',
-        margins: true,
-        sampleText:
-            'This paragraph should receive margins when the `--margins` modifier is applied.',
-    },
-    tags: ['options'],
+  args: {
+    variant: 'body',
+    margins: true,
+    sampleText:
+      'This paragraph should receive margins when the `--margins` modifier is applied.',
+  },
+  tags: ['options'],
 };
 
 /**
@@ -207,43 +209,43 @@ export const MarginsModifier: Story = {
  * - `p, li` - Body, size M
  */
 export const ProseContainer: Story = {
-    args: {
-        variant: 'body',
-        prose: true,
-    },
-    tags: ['options'],
-    render: () =>
-        html`<div
-            class="swc-Typography--prose typography-samples typography-samples--nogrid"
-        >
-            <h1>Semantic H1</h1>
-            <h2>Semantic H2</h2>
-            <p>
-                Semantic paragraph. Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Donec eleifend est mollis ligula lobortis,
-                tempus ultricies sapien lacinia.
-            </p>
-            <h3>Semantic H3</h3>
-            <p>
-                Semantic paragraph. Nulla ut turpis velit. Sed finibus dapibus
-                diam et sollicitudin.
-            </p>
-            <h4>Semantic H4</h4>
-            <p>
-                Semantic paragraph. Donec scelerisque orci sit amet venenatis
-                luctus.
-            </p>
-            <h2 class="swc-Heading">Heading default variant H2</h2>
-            <p class="swc-Body">
-                Body default variant paragraph. Phasellus in ipsum nec ante
-                elementum congue eget in leo. Morbi eleifend justo non rutrum
-                venenatis. Fusce cursus et lectus eu facilisis. Ut laoreet felis
-                in magna dignissim feugiat.
-            </p>
-            <ul>
-                <li>Semantic list item 1</li>
-                <li>Semantic list item 2</li>
-                <li>Semantic list item 3</li>
-            </ul>
-        </div>`,
+  args: {
+    variant: 'body',
+    prose: true,
+  },
+  tags: ['options'],
+  render: () => html`
+    <div
+      class="swc-Typography--prose typography-samples typography-samples--nogrid"
+    >
+      <h1>Semantic H1</h1>
+      <h2>Semantic H2</h2>
+      <p>
+        Semantic paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Donec eleifend est mollis ligula lobortis, tempus ultricies sapien
+        lacinia.
+      </p>
+      <h3>Semantic H3</h3>
+      <p>
+        Semantic paragraph. Nulla ut turpis velit. Sed finibus dapibus diam et
+        sollicitudin.
+      </p>
+      <h4>Semantic H4</h4>
+      <p>
+        Semantic paragraph. Donec scelerisque orci sit amet venenatis luctus.
+      </p>
+      <h2 class="swc-Heading">Heading default variant H2</h2>
+      <p class="swc-Body">
+        Body default variant paragraph. Phasellus in ipsum nec ante elementum
+        congue eget in leo. Morbi eleifend justo non rutrum venenatis. Fusce
+        cursus et lectus eu facilisis. Ut laoreet felis in magna dignissim
+        feugiat.
+      </p>
+      <ul>
+        <li>Semantic list item 1</li>
+        <li>Semantic list item 2</li>
+        <li>Semantic list item 3</li>
+      </ul>
+    </div>
+  `,
 };
