fdb02f526dd298a63e6b9e36aec1c2f13fce4446
rise-erpelding <54716846+rise-erpelding@users.noreply.github.com>
2026-03-02 15:33:45 -0600

    fix(colorfield): add handle class to color handle (#6057)

--- 1st-gen/packages/color-field/src/ColorField.ts
+++ 1st-gen/packages/color-field/src/ColorField.ts
@@ -59,6 +59,7 @@ export class ColorField extends TextfieldBase {
       ? html`
           <sp-color-handle
             size="m"
+            class="handle"
             color=${this.colorController.getColor('srgb').toString()}
           ></sp-color-handle>
         `
