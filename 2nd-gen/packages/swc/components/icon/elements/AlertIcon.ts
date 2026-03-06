e0725d1389c8139fb7f47fe6eb674c12e6b0b3c4
TarunAdobe <ttomar@adobe.com>
2026-03-06 14:49:29 +0530

    chore: fix lint issues

--- 2nd-gen/packages/swc/components/icon/elements/AlertIcon.ts
+++ 2nd-gen/packages/swc/components/icon/elements/AlertIcon.ts
@@ -13,10 +13,7 @@ import { html, TemplateResult } from 'lit';
 
 export const AlertIcon = (): TemplateResult => {
   return html`
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 36 36"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
       <path
         d="M17.127 2.579.4 32.512A1 1 0 0 0 1.272 34h33.456a1 1 0 0 0 .872-1.488L18.873 2.579a1 1 0 0 0-1.746 0ZM20 29.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-3a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5Zm0-6a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-12a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5Z"
       />
