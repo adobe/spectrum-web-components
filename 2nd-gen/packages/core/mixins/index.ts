f2149428f9546e7af5629d96b5977742315aef77
Ruben Carvalho <rcarvalho@adobe.com>
2026-04-01 13:49:25 +0200

    feat: move focusable

--- 2nd-gen/packages/core/mixins/index.ts
+++ 2nd-gen/packages/core/mixins/index.ts
@@ -10,6 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
+export { Focusable } from './focusable.js';
 export {
   ObserveSlotPresence,
   type SlotPresenceObservingInterface,
