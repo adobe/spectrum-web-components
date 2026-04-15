6dd7ed96d0ddc15e6ae137f41767368d3d709194
Cory Dransfeldt <cdransfeldt@adobe.com>
2026-04-15 15:59:52 -0700

    fix(storybook): correct test failure caused by string mismatch

--- 2nd-gen/packages/swc/components/progress-circle/test/progress-circle.a11y.spec.ts
+++ 2nd-gen/packages/swc/components/progress-circle/test/progress-circle.a11y.spec.ts
@@ -33,7 +33,8 @@ test.describe('Progress Circle - ARIA Snapshots', () => {
       'swc-progress-circle'
     );
     await expect(root).toMatchAriaSnapshot(`
-      - progressbar "Uploading document"
+      - progressbar "Processing request":
+        - img
     `);
   });
 
