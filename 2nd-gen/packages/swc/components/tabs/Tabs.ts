17284db7141651f76ca4dcb77490658a6c1ec03b
Ruben Carvalho <rcarvalho@adobe.com>
2026-05-11 14:33:25 +0200

    chore: update since version of components

--- 2nd-gen/packages/swc/components/tabs/Tabs.ts
+++ 2nd-gen/packages/swc/components/tabs/Tabs.ts
@@ -27,7 +27,7 @@ import styles from './tabs.css';
  * `aria-orientation` are co-located on the same inner element.
  *
  * @element swc-tabs
- * @since 0.0.1
+ * @since 2.0.0
  *
  * @slot - Tab items (`swc-tab` elements)
  * @slot tab-panel - Tab panel content (`swc-tab-panel` elements)
