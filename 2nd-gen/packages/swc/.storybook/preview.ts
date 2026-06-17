dd2c4b2a385a86cbad006d0776e2a3971d5ac0f3
Cory Dransfeldt <cdransfeldt@adobe.com>
2026-06-17 09:55:18 -0700

    fix(action-button): use inline svgs in storybook examples to fix extra space in icon only examples

--- 2nd-gen/packages/swc/.storybook/preview.ts
+++ 2nd-gen/packages/swc/.storybook/preview.ts
@@ -405,7 +405,10 @@ const preview = {
                   'Rendering and styling migration analysis',
                 ],
                 'Dropzone',
-                ['Rendering and styling migration analysis'],
+                [
+                  'Accessibility migration analysis',
+                  'Rendering and styling migration analysis',
+                ],
                 'Field group',
                 ['Rendering and styling migration analysis'],
                 'Field label',
