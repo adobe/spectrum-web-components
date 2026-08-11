d6c22e8f2468f3f1649d066d1edbf52e2ab2ac04
Cory Dransfeldt <cdransfeldt@adobe.com>
2026-08-11 08:38:22 -0700

    fix(patterns): use package specifier for ui-icon import

--- 2nd-gen/packages/swc/patterns/ai-toolkit/response-status/ResponseStatus.ts
+++ 2nd-gen/packages/swc/patterns/ai-toolkit/response-status/ResponseStatus.ts
@@ -30,7 +30,7 @@ import { MutationController } from '@lit-labs/observers/mutation-controller.js';
 import { SpectrumElement } from '@adobe/spectrum-wc-core/element/index.js';
 
 import '@adobe/spectrum-wc/components/icon/swc-icon.js';
-import '../../../components/ui-icons/swc-ui-icon.js';
+import '@adobe/spectrum-wc/components/ui-icons/swc-ui-icon.js';
 import '../pixel-loader/swc-pixel-loader.js';
 
 import { uniqueId } from '../../../utils/id.js';
