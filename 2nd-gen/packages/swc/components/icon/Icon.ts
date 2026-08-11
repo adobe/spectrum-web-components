ebe028bfebaaa083fa298e5651ec2789d128f110
Cory Dransfeldt <cdransfeldt@adobe.com>
2026-08-11 08:40:12 -0700

    chore(icon): restore status internal tag

--- 2nd-gen/packages/swc/components/icon/Icon.ts
+++ 2nd-gen/packages/swc/components/icon/Icon.ts
@@ -19,6 +19,7 @@ import styles from '../../stylesheets/_lit-styles/icon-base.css';
  * Minimal icon renderer that accepts slotted SVG markup.
  *
  * @element swc-icon
+ * @status internal
  * @since 2.0.0-beta.1
  *
  * @example
